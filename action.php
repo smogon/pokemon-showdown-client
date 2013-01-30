61c3f1fbe3d26d69b1e65435875edf5bab1d5650
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2013-01-30 04:16:48 -0700

    Remove glickotest and laddertest features

--- action.php
+++ action.php
@@ -204,27 +204,6 @@ foreach ($reqs as $reqData) {
 
 		die('success');
 		break;
-	case 'laddertest':
-		include_once 'lib/ntbb-ladder.lib.php';
-		$ladder = new NTBBLadder('showdown', 'ou');
-		
-		$p1 = array(
-			'rating' => array(
-				'r' => 1421.3542787803,
-				'rd' => 201.04843020314,
-				'sigma' => 0.059994925075002,
-				'rptime' => 1341446400,
-				'rpdata' => '[{"mu":0.63234332925469,"phi":0.27239037325046,"score":1},{"mu":1.7281504029414,"phi":0.43211783817514,"score":1},{"mu":0.93276078544053,"phi":0.99306537054533,"score":0},{"mu":-0.94051313144882,"phi":1.0099475282243,"score":1},{"mu":0.49643942459207,"phi":0.63523017522292,"score":1},{"mu":-1.0049237021831,"phi":0.3282791830483,"score":1},{"mu":1.5254097274879,"phi":0.87857256772749,"score":1},{"mu":0.66129185860516,"phi":0.8640590602575,"score":1},{"mu":-1.2436148187181,"phi":0.92373199886293,"score":1},{"mu":-0.64874832664816,"phi":0.55764845310102,"score":1},{"mu":-0.63452166982773,"phi":0.34511572273028,"score":1},{"mu":0.1021296183828,"phi":0.36117999082004,"score":1},{"mu":0.67700291340553,"phi":0.5099378019722,"score":1},{"mu":1.4314249415034,"phi":1.1022726006074,"score":1},{"mu":-0.6913699870422,"phi":1.4906684040673,"score":1},{"mu":-2.4139491282718,"phi":1.2766563943912,"score":1},{"mu":-0.54755058352569,"phi":0.19712357739302,"score":1},{"mu":1.6279574026323,"phi":0.20308162425787,"score":1},{"mu":1.6104933690428,"phi":0.20213766903132,"score":0},{"mu":0.81260576720117,"phi":0.14882872092356,"score":1},{"mu":-1.4771949264554,"phi":0.2576259670436,"score":1}]',
-			),
-		);
-
-		$ladder->update($p1, false, true);
-
-		var_export($p1);
-		
-		die();
-
-		break;
 	case 'ladderget':
 		include_once 'lib/ntbb-ladder.lib.php';
 		
@@ -281,19 +260,6 @@ foreach ($reqs as $reqData) {
 		
 		$ladder = new NTBBLadder($server['id'], $reqData['format']);
 		$out = $ladder->getTop();
-		break;
-	case 'glickotest':
-		include_once 'lib/ntbb-ladder.lib.php';
-		
-		$ladder = new NTBBLadder('test', 'OU');
-		
-		$p1 = $users->getUser('aeo1');
-		$p2 = $users->getUser('aeo2');
-		
-		$ladder->updateRating($p1, $p2, 1);
-		
-		var_export($p1);
-		die();
 		break;
 	default:
 		break;
