12689a93f1acc6296f57eebd12ee425cd9d8778d
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2013-02-13 14:29:13 -0700

    Fixes for `invalidate` option

--- customcss.php
+++ customcss.php
@@ -18,14 +18,14 @@ $timenow = time();
 $expiration = ($lastmodified ? $lastmodified : $timenow) + 3600;
 header('Expires: ' . gmdate('D, d M Y H:i:s T', $expiration));
 
-if (empty($_REQUEST['invalidate']) && $lastmodified && (($timenow - $lastmodified) < 3600)) {
+if (isset($_REQUEST['invalidate']) && $lastmodified && (($timenow - $lastmodified) < 3600)) {
 	// Don't check for modifications more than once an hour.
 	readfile($cssfile);
 	die();
 }
 
 $curl = curl_init($customcssuri);
-if ($lastmodified) {
+if ($lastmodified && empty($_REQUEST['invalidate'])) {
 	curl_setopt($curl, CURLOPT_HTTPHEADER, array(
 		'If-Modified-Since: ' . gmdate('D, d M Y H:i:s T', $lastmodified)
 	));
