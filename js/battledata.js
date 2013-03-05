dc7db0b211dad087cce0f2c6f6ea51a2495422e8
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2013-03-04 19:31:07 -0700

    Move custom colour for 'hexasexual' to 'Solace'

--- js/battledata.js
+++ js/battledata.js
@@ -111,7 +111,7 @@ function hashColor(name) {
 	if (name === 'haunter') hash = MD5('cathy');
 	if (name === 'ginganinja') hash = MD5('piratesandninjas');
 	if (name === 'aurora') hash = MD5('c6n6fek');
-	if (name === 'hexasexual') hash = MD5('amorlan');
+	if (name === 'solace') hash = MD5('amorlan');
 	if (name === 'cathy') return colorCache[name] = 'color:#ff5cb6;';
 	var H = parseInt(hash.substr(4, 4), 16) % 360;
 	var S = parseInt(hash.substr(0, 4), 16) % 50 + 50;
