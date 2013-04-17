b2801a5b82fa10c1ac19173d7fc0007cdaedcc6e
Guangcong Luo <guangcongluo@gmail.com>
2013-04-17 14:46:25 -0700

    Don't crash getIcon without pokedex-mini loaded

--- js/battledata.js
+++ js/battledata.js
@@ -710,9 +710,9 @@ var Tools = {
 		var id = toId(pokemon);
 		if (pokemon && pokemon.species) id = toId(pokemon.species);
 		if (pokemon && pokemon.volatiles && pokemon.volatiles.formechange && !pokemon.volatiles.transform) id = toId(pokemon.volatiles.formechange[2]);
-		if (pokemon && pokemon.num) num = pokemon.num;
-		else if (BattlePokemonSprites[id] && BattlePokemonSprites[id].num) num = BattlePokemonSprites[id].num;
-		else if (exports.BattlePokedex[id] && exports.BattlePokedex[id].num) num = exports.BattlePokedex[id].num;
+		if (pokemon && pokemon.num !== undefined) num = pokemon.num;
+		else if (window.BattlePokemonSprites && BattlePokemonSprites[id] && BattlePokemonSprites[id].num) num = BattlePokemonSprites[id].num;
+		else if (window.BattlePokedex[id] && BattlePokedex[id].num) num = BattlePokedex[id].num;
 		if (num < 0) num = 0;
 		var altNums = {
 			"rotomfan": 699,
