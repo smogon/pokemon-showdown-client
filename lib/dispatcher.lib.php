17b8917d0b4d50716757ff286022990d11cbaa86
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2013-04-22 18:34:54 -0600

    Update PO userstats whenever the main server reports userstats

--- lib/dispatcher.lib.php
+++ lib/dispatcher.lib.php
@@ -264,6 +264,9 @@ class DefaultActionHandler {
 			$db->query(
 				"INSERT INTO `ntbb_userstatshistory` (`date`, `usercount`) " .
 				"VALUES ('" . $db->escape($date) . "', '" . $db->escape($usercount) . "')");
+
+			include_once dirname(__FILE__) . '../../pokemonshowdown.com/lib/poregistry.lib.php';
+			PORegistry::updateUserStats();
 		}
 		$dispatcher->setPrefix(''); // No need for prefix since only usable by server.
 	}
