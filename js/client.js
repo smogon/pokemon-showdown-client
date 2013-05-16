07b863b8e11553c40b3fb4f8623576a4bb678419
Guangcong Luo <guangcongluo@gmail.com>
2013-05-15 23:09:47 -0700

    Workaround for a race condition while loading

--- js/client.js
+++ js/client.js
@@ -36,6 +36,14 @@
 		return false;
 	};
 
+	// placeholder until the real chart loads
+	window.Chart = {
+		pokemonRow: function() {},
+		itemRow: function() {},
+		abilityRow: function() {},
+		moveRow: function() {}
+	};
+
 	var User = this.User = Backbone.Model.extend({
 		defaults: {
 			name: '',
