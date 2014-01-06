5268d6b9bec0856e1c0b12d051e458d08d7764a1
V4Victini <gugugtutuuttu@aol.com>
2014-01-06 15:25:50 -0500

    Readd previous condition
    Backwards Compatibility

--- js/client-battle.js
+++ js/client-battle.js
@@ -379,7 +379,7 @@
 					} else {
 						controls += movebuttons;
 					}
-					if (switchables[pos].canMegaEvo) {
+					if (switchables[pos].canMegaEvo || Tools.getItem(switchables[pos].item).megaStone && !switchables[pos].volatiles.formechange) {
 						controls += '<br /><label><input type="checkbox" name="megaevo" /> Mega evolution</label>'
 					}
 					controls += '<div style="clear:left"></div>';
