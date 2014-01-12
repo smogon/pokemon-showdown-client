026ff8b4c7e53e525e0f7cd30532ef388ae568e0
sirDonovan <quivistis@gmail.com>
2014-01-12 00:11:06 -0600

    Update client-chat.js
    Links created with Google and Wikipedia commands are now removable upon muting/locking/banning.

--- js/client-chat.js
+++ js/client-chat.js
@@ -822,6 +822,9 @@
 					// so that it can be included in the scrollback buffer.
 					$('.message-link-' + toId(row[1])).each(function() {
 						$(this).replaceWith($(this).html());
+					})
+					$('a.searching').each(function() {
+						$(this).replaceWith($(this).html());
 					});
 					break;
 
