6b3466af4c9d46dd70b7bd4b11a9fe5cd4eb0750
Guangcong Luo <guangcongluo@gmail.com>
2014-02-27 19:26:41 -0600

    Sync data

--- data/pokedex-mini.js
+++ data/pokedex-mini.js
@@ -806,5 +806,5 @@ BattlePokemonSprites = {
 	"noivern":{num:715, front:{ani:{w:184,h:156}}, back:{ani:{w:162,h:157}}},
 	"xerneas":{num:716, front:{ani:{w:106,h:127}}, back:{ani:{w:94,h:128}}},
 	"yveltal":{num:717, front:{ani:{w:201,h:188}}, back:{ani:{w:186,h:194}}},
-	"zygarde":{num:71, front:{ani:{w:96,h:107}}, back:{ani:{w:113,h:123}}}
+	"zygarde":{num:718, front:{ani:{w:96,h:107}}, back:{ani:{w:113,h:123}}}
 };
