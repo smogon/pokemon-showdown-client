c9ad1f5808decc97e0deb339a36d89efb2b18c95
Oesher Ayenew <Oiawesome99@yahoo.com>
2014-03-07 22:55:58 -0800

    Floette-E Alias
    "22:43<Oiawesome>should I pull req floette-eternal-flower alias as floette-e?" "<22:44>Zarel	k"

--- data/aliases.js
+++ data/aliases.js
@@ -76,6 +76,7 @@ exports.BattleAliases = {
 	"wormadamground": "Wormadam-Sandy",
 	"wormadams": "Wormadam-Trash",
 	"wormadamsteel": "Wormadam-Trash",
+	"floettee": "Floette-Eternal-Flower"
 
 	// base formes
 	"nidoranfemale": "Nidoran-F",
