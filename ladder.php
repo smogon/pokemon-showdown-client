64c8934c911e3631e3b3bcbeca86bcf625e483fd
Antar1011 <summer.kenster@gmail.com>
2014-03-12 10:42:27 -0400

    No more ARMS
    COIL only

--- ladder.php
+++ ladder.php
@@ -21,7 +21,7 @@ $ladder = new NTBBLadder($serverid, $formatid);
 	<table>
 		<tr>
 			<th></th><th>Name</th><th><abbr title="Elo rating">Elo</abbr></th><th><abbr title="user's percentage chance of winning a random battle (aka GLIXARE)">GXE</abbr></th><th><abbr title="Glicko-1 rating system: rating&#177;deviation (provisional if deviation>100)">Glicko-1</abbr></th>
-			<th><abbr title="COIL score (just testing)">MYSTERY <br />RATING A</abbr></th><th><abbr title="ARMS score (just testing)">MYSTERY <br />RATING B</abbr></th>
+			<th>COIL</th>
 		</tr>
 <?php
 
@@ -45,8 +45,7 @@ foreach ($toplist as $row)
 		<tr<?php /* if (floatval($row['rprd']) > 100) echo ' style="color:#999"'; */ ?>>
 			<td><?php echo $i; ?></td><td><?php echo htmlspecialchars($row['username']); ?></td><td><strong><?php echo round($row['acre']); ?></strong></td><td><?php echo number_format($row['gxe'],1); ?></td>
 			<td><?php echo '<em>'.round($row['rpr']).'<small> &#177; '.round($row['rprd']).'</small></em>'; /* if (floatval($row['rprd']) > 100) echo ' <small>(provisional)</small>'; */ ?></td>
-			<td><?php echo number_format($N ? 40*$row['gxe']*pow(2.0,-34.0/$N) : 0,1,'.',''); ?></td>
-			<td><?php echo number_format(1000+(2*$row['gxe']-100)*$N/4,1,'.',''); ?></td>
+			<td><?php echo number_format($N ? 40*$row['gxe']*pow(2.0,-43.0/$N) : 0,1,'.',''); ?></td>
 		</tr>
 <?php
 }
