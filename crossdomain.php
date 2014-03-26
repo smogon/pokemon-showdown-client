e0153dd3ff1443f076fa302eb4ae3bea3c0703d8
Guangcong Luo <guangcongluo@gmail.com>
2014-03-26 12:31:28 -0500

    Update links to jQuery and SockJS

--- crossdomain.php
+++ crossdomain.php
@@ -78,7 +78,7 @@ if (isset($PokemonServers[$config['host']])) {
 header('P3P: CP="NOI CUR ADM DEV COM NAV STA OUR IND"');
 ?>
 <!DOCTYPE html>
-<script src="/js/lib/jquery-2.0.0.min.js"></script>
+<script src="/js/lib/jquery-2.1.0.min.js"></script>
 <script src="/js/lib/jquery-cookie.js"></script>
 <script src="/js/lib/jquery.json-2.3.min.js"></script>
 <body>
