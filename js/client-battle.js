smogon/pokemon-showdown-client

commit 24c07d32713ce2fd715c42bc6ff3680a591b5817

Suppress crash on move request with all actives fainted
There's a weird bug in which it's possible to open a choose-move
screen when the client believes that all your active Pokemon are
fainted (this is not true). It seems to involve having a team
with multiple Pokemon with the same name, multiple Pokemon
with the same species, and/or Illusion (in case it was unclear,
this is a Hackmons-specific bug).

I don't have time to look at it in depth, but this commit
suppresses it enough to make PS playable instead of crashing
in that situation.

js/client-battle.js
@@ -252,7 +252,7 @@
 							choices: [],
 							switchFlags: {}
 						}
-						while (switchables[this.choice.choices.length] && switchables[this.choice.choices.length].fainted) {
+						if (this.choice.choices.length > 1) while (switchables[this.choice.choices.length] && switchables[this.choice.choices.length].fainted) {
 							this.choice.choices.push('pass');
 						}
 					}

js/battlescene.js
@@ -1,0 +1,14 @@
+/**
+ * Battle Scene library
+ * Pokemon Showdown - http://pokemonshowdown.com/
+ *
+ * @license GPLv2 <http://www.gnu.org/licenses/gpl-2.0.html>
+ */
+
+function BattleScene {
+	//
+}
+
+function BattleSprite {
+	//
+}

commit db7a17806fc86e1b41691e10d5af61a2ebcad571

Fix asking for moves for fainted pokemon
The bug was introduced as a typo in 24c07d32

js/client-battle.js
@@ -252,7 +252,7 @@
 							choices: [],
 							switchFlags: {}
 						}
-						if (this.choice.choices.length > 1) while (switchables[this.choice.choices.length] && switchables[this.choice.choices.length].fainted) {
+						if (this.battle.mySide.active.length > 1) while (switchables[this.choice.choices.length] && switchables[this.choice.choices.length].fainted) {
 							this.choice.choices.push('pass');
 						}
 					}