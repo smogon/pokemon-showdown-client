cec23bdd2072c52663b228ebfb3a9dfd4beda3db
Guangcong Luo <guangcongluo@gmail.com>
2014-05-04 13:40:57 -0500

    Fix syntax error from 32b4ebfe8c

--- js/client-chat.js
+++ js/client-chat.js
@@ -572,7 +572,6 @@
 									buffer += '<td>'+Math.round(40.0*parseFloat(row.gxe)*Math.pow(2.0,-17.0/N),0)+'</td>';
 								} else if (row.formatid === 'smogondoubles' || row.formatid === 'smogondoublessuspecttest') {
 									buffer += '<td>'+Math.round(40.0*parseFloat(row.gxe)*Math.pow(2.0,-15.0/N),0)+'</td>';
-								}
 								} else {
 									buffer += '<td>--</td>';
 								}
