smogon/pokemon-showdown-client

commit acd4900e819a27a731ffe844dcb2c9064bcb742b

Export IVs and EVs with proper stat names
Also have them exported in order: HP, Atk, Def, SpA, SpD, Spe

js/client-teambuilder.js
@@ -2049,7 +2049,7 @@
 					text += 'Happiness: '+curSet.happiness+"\n";
 				}
 				var first = true;
-				for (var j in curSet.evs) {
+				for (var j in BattleStatNames) {
 					if (!curSet.evs[j]) continue;
 					if (first) {
 						text += 'EVs: ';
@@ -2057,7 +2057,7 @@
 					} else {
 						text += ' / ';
 					}
-					text += ''+curSet.evs[j]+' '+BattlePOStatNames[j];
+					text += ''+curSet.evs[j]+' '+BattleStatNames[j];
 				}
 				if (!first) {
 					text += "\n";
@@ -2094,7 +2094,7 @@
 						}
 					}
 					if (!defaultIvs) {
-						for (var stat in curSet.ivs) {
+						for (var stat in BattleStatNames) {
 							if (typeof curSet.ivs[stat] === 'undefined' || curSet.ivs[stat] == 31) continue;
 							if (first) {
 								text += 'IVs: ';
@@ -2102,7 +2102,7 @@
 							} else {
 								text += ' / ';
 							}
-							text += ''+curSet.ivs[stat]+' '+BattlePOStatNames[stat];
+							text += ''+curSet.ivs[stat]+' '+BattleStatNames[stat];
 						}
 					}
 				}

commit 7e2ed6032bff5cbc750cdb56c564a693b88850a3

Properly export with proper stat names.
Addresses the rare crash issue introduced in acd4900e819a27a731ffe844dcb2c9064bcb742b.

js/client-teambuilder.js
@@ -2049,15 +2049,17 @@
 					text += 'Happiness: '+curSet.happiness+"\n";
 				}
 				var first = true;
-				for (var j in curSet.evs) {
-					if (!curSet.evs[j]) continue;
-					if (first) {
-						text += 'EVs: ';
-						first = false;
-					} else {
-						text += ' / ';
+				if (curSet.evs) {
+					for (var j in BattleStatNames) {
+						if (!curSet.evs[j]) continue;
+						if (first) {
+							text += 'EVs: ';
+							first = false;
+						} else {
+							text += ' / ';
+						}
+						text += ''+curSet.evs[j]+' '+BattleStatNames[j];
 					}
-					text += ''+curSet.evs[j]+' '+BattleStatNames[j];
 				}
 				if (!first) {
 					text += "\n";
@@ -2077,7 +2079,7 @@
 								alert("That is not a valid Hidden Power type.");
 								continue;
 							}
-							for (var stat in curSet.ivs) {
+							for (var stat in BattleStatNames) {
 								if ((curSet.ivs[stat]===undefined?31:curSet.ivs[stat]) !== (exports.BattleTypeChart[hpType].HPivs[stat]||31)) {
 									defaultIvs = false;
 									break;