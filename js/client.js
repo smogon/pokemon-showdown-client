be412e8d88d0f74767347364e281843c543277d1
Konrad Borowski <x.fix@o2.pl>
2015-03-11 22:47:39 +0100

    Add Twitch emotes.

--- js/client.js
+++ js/client.js
@@ -2529,6 +2529,7 @@
 			buf += '<p><label class="optlabel"><input type="checkbox" name="me" ' + (cur.hideme ? 'checked' : '') + ' /> Suppress <code>/me</code> <em>action formatting</em></label></p>';
 			buf += '<p><label class="optlabel"><input type="checkbox" name="spoiler" ' + (cur.hidespoiler ? 'checked' : '') + ' /> Suppress spoiler hiding</label></p>';
 			buf += '<p><label class="optlabel"><input type="checkbox" name="links" ' + (cur.hidelinks ? 'checked' : '') + ' /> Suppress clickable links</label></p>';
+			buf += '<p><label class="optlabel"><input type="checkbox" name="twitchemotes" ' + (cur.hidetwitchemotes ? 'checked' : '') + ' /> Suppress Twitch emotes</label></p>';
 			buf += '<p><button name="close">Close</button></p>';
 			this.$el.html(buf);
 		},
