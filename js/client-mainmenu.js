3ab229fa1acef52033c8766b8e7d259c6c70c67f
Guangcong Luo <guangcongluo@gmail.com>
2015-06-12 14:27:47 -0400

    Revert "Make sure Staff and Upper Staff are first"
    Now that autojoin saves the position of Staff and Upper Staff, this
    is no longer necessary.
    
    This reverts commit e5b3326703eab2f666f5d7488c2fcfd2f1589927.

--- js/client-mainmenu.js
+++ js/client-mainmenu.js
@@ -313,7 +313,6 @@
 				var handlerRoom = app.curSideRoom;
 				if (!handlerRoom) {
 					for (var roomid in app.rooms) {
-						if (!app.rooms[roomid]) continue;
 						if (!app.rooms[roomid].handleTabComplete) continue;
 						handlerRoom = app.rooms[roomid];
 						break;
