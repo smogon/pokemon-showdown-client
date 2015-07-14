53a1c2ad9ed7cd4b7ce3e38259a9ceb4b56bf799
Guangcong Luo <guangcongluo@gmail.com>
2015-07-14 15:03:40 -0400

    Support HTTPS PS-to-desktop team dragging
    Chrome is dumb and doesn't support data URLs in HTTPS, so we have
    to round-trip through action.php

--- action.php
+++ action.php
@@ -9,6 +9,16 @@ License: GPLv2 or later
 
 error_reporting(E_ALL);
 
+if (@$_GET['act'] === 'dlteam') {
+	header("Content-Type: text/plain");
+	if (substr($_SERVER['HTTP_REFERER'], 0, 32) !== 'https://play.pokemonshowdown.com') {
+		// since this is only to support Chrome on HTTPS, we can get away with a very specific referer check
+		die("access denied");
+	}
+	echo base64_decode(@$_GET['team']);
+	die();
+}
+
 include_once '../pokemonshowdown.com/lib/ntbb-session.lib.php';
 include_once '../pokemonshowdown.com/config/servers.inc.php';
 include_once 'lib/dispatcher.lib.php';
