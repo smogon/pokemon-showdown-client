smogon/pokemon-showdown-client

commit 128a8bc9bf8f35b18a8dc06a7a2929aefabbd0b3

Handle |refresh| message globally
Now the /refreshpage command will no longer have its effects limited to users in Lobby.

js/client.js
@@ -1080,6 +1080,11 @@
 					}
 				}
 
+			case 'refresh':
+				// refresh the page
+				document.location.reload(true);
+				break;
+
 			/* fall through */
 			default:
 				// the messagetype wasn't in our list of recognized global

js/client-chat.js
@@ -1161,10 +1161,6 @@
 					this.addJoinLeave('rename', row[1], row[2], true);
 					break;
 
-				case 'refresh':
-					// refresh the page
-					document.location.reload(true);
-					break;
 
 				case 'users':
 					this.parseUserList(row[1]);

commit 7c524be7dd009ce9484db46e518320af3b0a4ad4

Fix backwards compat regression from 128a8bc wrt `|chat|`

js/client.js
@@ -1071,6 +1071,11 @@
 				this.addPopupMessage(parts.slice(2).join('|'));
 				break;
 
+			case 'refresh':
+				// refresh the page
+				document.location.reload(true);
+				break;
+
 			case 'c':
 			case 'chat':
 				if (parts[1] === '~') {
@@ -1079,11 +1084,6 @@
 						break;
 					}
 				}
-
-			case 'refresh':
-				// refresh the page
-				document.location.reload(true);
-				break;
 
 			/* fall through */
 			default: