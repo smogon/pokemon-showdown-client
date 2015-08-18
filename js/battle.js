93abac306bda3c30e4239678e979e9542afcae06
urkerab <urkerab@users.noreply.github.com>
2015-08-18 12:19:03 +0100

    Consistently add and remove the hp colour classes

--- js/battle.js
+++ js/battle.js
@@ -1955,7 +1955,7 @@ var Side = (function () {
 			});
 			if (hpcolor === 'g') $hp.removeClass('hp-yellow hp-red');
 			else if (hpcolor === 'y') $hp.removeClass('hp-red').addClass('hp-yellow');
-			else $hp.addClass('hp-red');
+			else $hp.addClass('hp-yellow hp-red');
 			this.updateHPText(pokemon);
 		}
 		if (updatePrevhp) {
@@ -1963,7 +1963,7 @@ var Side = (function () {
 			$prevhp.css('width', pokemon.hpWidth(150) + 1);
 			if (hpcolor === 'g') $prevhp.removeClass('prevhp-yellow prevhp-red');
 			else if (hpcolor === 'y') $prevhp.removeClass('prevhp-red').addClass('prevhp-yellow');
-			else $prevhp.addClass('prevhp-red');
+			else $prevhp.addClass('prevhp-yellow prevhp-red');
 		}
 		var status = '';
 		if (pokemon.status === 'brn') {
@@ -2897,8 +2897,7 @@ var Battle = (function () {
 			$hp.addClass('hp-yellow');
 		};
 		if (hpcolor === 'r') callback = function () {
-			$hp.addClass('hp-yellow');
-			$hp.addClass('hp-red');
+			$hp.addClass('hp-yellow hp-red');
 		};
 
 		if (this.fastForward) {
@@ -2926,8 +2925,7 @@ var Battle = (function () {
 		var hpcolor = pokemon.getHPColor();
 		var callback;
 		if (hpcolor === 'g') callback = function () {
-			$hp.removeClass('hp-yellow');
-			$hp.removeClass('hp-red');
+			$hp.removeClass('hp-yellow hp-red');
 		};
 		if (hpcolor === 'y') callback = function () {
 			$hp.removeClass('hp-red');
