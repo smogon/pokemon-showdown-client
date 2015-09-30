c7a58f7ab90030f36f124c1b2cb1838f53fd1dd5
Irraquated <hookerjackson22@gmail.com>
2015-10-01 02:00:43 +1000

    Focus PS! when disconnection notif. is clicked

--- js/client.js
+++ js/client.js
@@ -440,7 +440,10 @@
 				// Display a desktop notification if the user won't immediately see the popup.
 				if ((self.popups.length || !self.focused) && window.Notification) {
 					self.rooms[''].requestNotifications();
-					new Notification("Reconnect to Showdown!", {lang: 'en', body: "You have been disconnected \u2014 possibly because the server was restarted."});
+					var disconnect = new Notification("Reconnect to Showdown!", {lang: 'en', body: "You have been disconnected \u2014 possibly because the server was restarted."});
+					disconnect.onclick = function (e) {
+						window.focus();
+					};
 				}
 				self.reconnectPending = true;
 				if (!self.popups.length) self.addPopup(ReconnectPopup);
