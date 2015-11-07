c0f18d1ff4989f3dc62bca02af18268a594e5a6c
Guangcong Luo <guangcongluo@gmail.com>
2015-11-06 21:39:51 -0500

    Fix Will-O-Wisp miss animation
    Will-O-Wisp's miss animation is really frustrating because it's
    very hard to distinguish from its hit animation. The new version
    now doesn't do the 'orbit' if it misses.

--- data/graphics.js
+++ data/graphics.js
@@ -7092,6 +7092,7 @@ var BattleMoveAnims = {
 				opacity: .7,
 				time: 500
 			}, 'decel', 'fade');
+			if (defender.isMissedPokemon) return;
 			battle.showEffect('fireball', {
 				x: defender.leftof(40),
 				y: defender.y+15,
