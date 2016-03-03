8336eaad01d0849f415e7b70b69ed4267086c4a6
The Immortal <the_immortal123@live.com>
2016-03-04 00:52:24 +0400

    Add Monotype COIL

--- js/client-chat.js
+++ js/client-chat.js
@@ -745,8 +745,8 @@
 									buffer += '<td>' + Math.round(40.0 * parseFloat(row.gxe) * Math.pow(2.0, -9.0 / N), 0) + '</td>';
 								} else if (row.formatid === 'doublesoucurrent' || row.formatid === 'doublesoususpecttest') {
 									buffer += '<td>' + Math.round(40.0 * parseFloat(row.gxe) * Math.pow(2.0, -12.0 / N), 0) + '</td>';
-								} else if (row.formatid === '1v1') {
-									buffer += '<td>' + Math.round(40.0 * parseFloat(row.gxe) * Math.pow(2.0, -20.0 / N), 0) + '</td>';
+								} else if (row.formatid === 'monotype') {
+									buffer += '<td>' + Math.round(40.0 * parseFloat(row.gxe) * Math.pow(2.0, -8.0 / N), 0) + '</td>';
 								} else {
 									buffer += '<td>--</td>';
 								}
