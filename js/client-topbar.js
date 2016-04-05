b961d1030cf654df6275c1b5c55e7804c0765748
Guangcong Luo <guangcongluo@gmail.com>
2016-04-04 22:59:04 -0400

    Fix avatar chooser alignment

--- js/client-topbar.js
+++ js/client-topbar.js
@@ -589,8 +589,8 @@
 
 			buf += '<div class="avatarlist">';
 			for (var i = 1; i <= 293; i++) {
-				var offset = '-' + (((i - 1) % 16) * 80) + 'px -' + (Math.floor((i - 1) / 16) * 80) + 'px';
-				buf += '<button name="setAvatar" value="' + i + '" style="background-position:' + offset + '"' + (i === cur ? ' class="cur"' : '') + '></button>';
+				var offset = '-' + (((i - 1) % 16) * 80 + 1) + 'px -' + (Math.floor((i - 1) / 16) * 80 + 1) + 'px';
+				buf += '<button name="setAvatar" value="' + i + '" style="background-position:' + offset + '"' + (i === cur ? ' class="cur"' : '') + ' title="/avatar ' + i + '"></button>';
 			}
 			buf += '</div><div style="clear:left"></div>';
 
