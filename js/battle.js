97f471812124cd1c58e3492c0ad909fedfafbd16
Layell <iancclail@gmail.com>
2016-04-10 15:41:53 -0400

    bwgfx will set backgrounds to gen 5 backgrounds

--- js/battle.js
+++ js/battle.js
@@ -2378,6 +2378,7 @@ var Battle = (function () {
 			else if (this.gen <= 4) this.backdropImage = 'fx/' + BattleBackdropsFour[Math.floor(Math.random() * BattleBackdropsFour.length)];
 			else if (this.gen <= 5) this.backdropImage = 'fx/' + BattleBackdropsFive[Math.floor(Math.random() * BattleBackdropsFive.length)];
 		}
+		if (this.gen <=6 && Tools.prefs('bwgfx')) this.backdropImage = 'fx/' + BattleBackdropsFive[Math.floor(Math.random() * BattleBackdropsFive.length)];
 		if (this.bgElem) this.bgElem.css('background-image', 'url(' + Tools.resourcePrefix + '' + this.backdropImage + ')');
 	};
 	Battle.prototype.reset = function (dontResetSound) {
