0790cf9479db6243b65640d292170cd2dd84427a
Ivo Julca <ivojulca@hotmail.com>
2016-06-03 23:35:22 -0500

    Refactor room list for better readability

--- js/client-rooms.js
+++ js/client-rooms.js
@@ -8,7 +8,9 @@
 		isSideRoom: true,
 		initialize: function () {
 			this.$el.addClass('ps-room-light').addClass('scrollable');
-			var buf = '<div class="pad"><button class="button" style="float:right;font-size:10pt;margin-top:3px" name="closeHide"><i class="fa fa-caret-right"></i> Hide</button><div class="roomlisttop"></div><div class="roomlist"><p><em style="font-size:20pt">Loading...</em></p></div><div class="roomlist"></div><p><button name="joinRoomPopup" class="button">Join other room</button></p></div>';
+			var buf = '<div class="pad"><button class="button" style="float:right;font-size:10pt;margin-top:3px" name="closeHide"><i class="fa fa-caret-right"></i> Hide</button>';
+			buf += '<div class="roomlisttop"></div><div class="roomlist"><p><em style="font-size:20pt">Loading...</em></p></div><div class="roomlist"></div>';
+			buf += '<p><button name="joinRoomPopup" class="button">Join other room</button></p></div>';
 			this.$el.html(buf);
 			app.on('response:rooms', this.update, this);
 			app.send('/cmd rooms');
@@ -51,41 +53,25 @@
 				app.roomsFirstOpen = 2;
 			}
 		},
+		renderRoomBtn: function (roomData) {
+			var id = toId(roomData.title);
+			return '<div><a href="' + app.root + id + '" class="ilink"><small style="float:right">(' + Number(roomData.userCount) + ' users)</small><strong><i class="fa fa-comment-o"></i> ' + Tools.escapeHTML(roomData.title) + '<br /></strong><small>' + Tools.escapeHTML(roomData.desc || '') + '</small></a></div>';
+		},
+		compareRooms: function (roomA, roomB) {
+			return roomB.userCount - roomA.userCount;
+		},
 		updateRoomList: function () {
 			var rooms = app.roomsData;
-			var buf = '';
 
 			if (rooms.userCount) {
 				var userCount = Number(rooms.userCount);
 				var battleCount = Number(rooms.battleCount);
-				buf += '<table class="roomcounters" border="0" cellspacing="0" cellpadding="0" width="100%"><tr><td>';
-				buf += '<span style="' + Tools.getPokemonIcon('meloetta', true) + ';display:inline-block;vertical-align:middle;transform:scaleX(-1);webkit-transform:scaleX(-1);" class="picon" title="Meloetta is PS\'s mascot! The Aria forme is about using its voice, and represents our chatrooms."></span> <button class="button" name="finduser" title="Find an online user"><strong>' + userCount + '</strong> ' + (userCount == 1 ? 'user' : 'users') + ' online</button></td><td>';
-				buf += '<button class="button" name="roomlist" title="Watch an active battle"><strong>' + battleCount + '</strong> active ' + (battleCount == 1 ? 'battle' : 'battles') + '</button> <span style="' + Tools.getPokemonIcon('meloetta-pirouette') + ';display:inline-block;vertical-align:middle" class="picon" title="Meloetta is PS\'s mascot! The Pirouette forme is Fighting-type, and represents our battles."></span>';
-				buf += '</td></tr></table>';
-				this.$('.roomlisttop').html(buf);
+				var leftSide = '<span style="' + Tools.getPokemonIcon('meloetta', true) + ';" class="picon icon-left" title="Meloetta is PS\'s mascot! The Aria forme is about using its voice, and represents our chatrooms."></span> <button class="button" name="finduser" title="Find an online user"><strong>' + userCount + '</strong> ' + (userCount == 1 ? 'user' : 'users') + ' online</button>';
+				var rightSide = '<button class="button" name="roomlist" title="Watch an active battle"><strong>' + battleCount + '</strong> active ' + (battleCount == 1 ? 'battle' : 'battles') + '</button> <span style="' + Tools.getPokemonIcon('meloetta-pirouette') + '" class="picon icon-right" title="Meloetta is PS\'s mascot! The Pirouette forme is Fighting-type, and represents our battles."></span>';
+				this.$('.roomlisttop').html('<table class="roomcounters" border="0" cellspacing="0" cellpadding="0" width="100%"><tr><td>' + leftSide + '</td><td>' + rightSide + '</td></tr></table>');
 			}
-
-			buf = '';
-			buf += '<h2 class="rooms-officialchatrooms">Official chat rooms</h2>';
-			for (var i = 0; i < rooms.official.length; i++) {
-				var roomData = rooms.official[i];
-				var id = toId(roomData.title);
-				buf += '<div><a href="' + app.root + id + '" class="ilink"><small style="float:right">(' + Number(roomData.userCount) + ' users)</small><strong><i class="fa fa-comment-o"></i> ' + Tools.escapeHTML(roomData.title) + '<br /></strong><small>' + Tools.escapeHTML(roomData.desc || '') + '</small></a></div>';
-			}
-			this.$('.roomlist').first().html(buf);
-
-			buf = '';
-			buf += '<h2 class="rooms-chatrooms">Chat rooms</h2>';
-			rooms.chat.sort(function (a, b) {
-				return b.userCount - a.userCount;
-			});
-			for (var i = 0; i < rooms.chat.length; i++) {
-				var roomData = rooms.chat[i];
-				var id = toId(roomData.title);
-				var escapedDesc = Tools.escapeHTML(roomData.desc || '');
-				buf += '<div><a href="' + app.root + id + '" class="ilink"><small style="float:right">(' + Number(roomData.userCount) + ' users)</small><strong><i class="fa fa-comment-o"></i> ' + Tools.escapeHTML(roomData.title) + '<br /></strong><small>' + escapedDesc + '</small></a></div>';
-			}
-			this.$('.roomlist').last().html(buf);
+			this.$('.roomlist').first().html('<h2 class="rooms-officialchatrooms">Official chat rooms</h2>' + _.map(rooms.official, this.renderRoomBtn).join(""));
+			this.$('.roomlist').last().html('<h2 class="rooms-chatrooms">Chat rooms</h2>' + _.map(rooms.chat.sort(this.compareRooms), this.renderRoomBtn).join(""));
 		},
 		roomlist: function () {
 			app.joinRoom('battles');
@@ -160,6 +146,20 @@
 			app.send('/cmd roomlist');
 			this.lastUpdate = new Date().getTime();
 		},
+		renderRoomBtn: function (id, roomData, matches) {
+			var format = (matches[1] || '');
+			var formatBuf = '';
+			if (roomData.minElo) formatBuf += '<small style="float:right">(' + (typeof roomData.minElo === 'number' ? 'rated: ' : '') + Tools.escapeHTML(roomData.minElo) + ')</small>';
+			formatBuf += (format ? '<small>[' + Tools.escapeFormat(format) + ']</small><br />' : '');
+			var roomDesc = formatBuf + '<em class="p1">' + Tools.escapeHTML(roomData.p1) + '</em> <small class="vs">vs.</small> <em class="p2">' + Tools.escapeHTML(roomData.p2) + '</em>';
+			if (!roomData.p1) {
+				matches = id.match(/[^0-9]([0-9]*)$/);
+				roomDesc = formatBuf + 'empty room ' + matches[1];
+			} else if (!roomData.p2) {
+				roomDesc = formatBuf + '<em class="p1">' + Tools.escapeHTML(roomData.p1) + '</em>';
+			}
+			return '<div><a href="' + app.root + id + '" class="ilink">' + roomDesc + '</a></div>';
+		},
 		update: function (data) {
 			if (!data && !this.data) {
 				if (app.isDisconnected) {
@@ -170,47 +170,30 @@
 				return;
 			}
 			this.$('button[name=refresh]')[0].disabled = false;
-			if (!data) {
-				data = this.data;
-			} else {
-				this.data = data;
-			}
-			var buf = '';
 
-			var i = 0;
-			for (var id in data.rooms) {
-				var roomData = data.rooms[id];
+			// Synchronize stored room data with incoming data
+			if (data) this.data = data;
+			var rooms = this.data.rooms;
+
+			var buf = [];
+			for (var id in rooms) {
+				var roomData = rooms[id];
 				var matches = ChatRoom.parseBattleID(id);
-				if (!matches) {
-					continue; // bogus room ID could be used to inject JavaScript
+				// bogus room ID could be used to inject JavaScript
+				if (!matches || this.format && matches[1] !== this.format) {
+					continue;
 				}
-				var format = (matches[1] || '');
-				if (this.format && format !== this.format) continue;
-				var formatBuf = '';
-				if (roomData.minElo) formatBuf += '<small style="float:right">(' + (typeof roomData.minElo === 'number' ? 'rated: ' : '') + Tools.escapeHTML(roomData.minElo) + ')</small>';
-				formatBuf += (format ? '<small>[' + Tools.escapeFormat(format) + ']</small><br />' : '');
-				var roomDesc = formatBuf + '<em class="p1">' + Tools.escapeHTML(roomData.p1) + '</em> <small class="vs">vs.</small> <em class="p2">' + Tools.escapeHTML(roomData.p2) + '</em>';
-				if (!roomData.p1) {
-					matches = id.match(/[^0-9]([0-9]*)$/);
-					roomDesc = formatBuf + 'empty room ' + matches[1];
-				} else if (!roomData.p2) {
-					roomDesc = formatBuf + '<em class="p1">' + Tools.escapeHTML(roomData.p1) + '</em>';
-				}
-				buf += '<div><a href="' + app.root + id + '" class="ilink">' + roomDesc + '</a></div>';
-				i++;
+				buf.push(this.renderRoomBtn(id, roomData, matches));
 			}
 
-			if (!i) {
-				buf = '<p>No ' + Tools.escapeFormat(this.format) + ' battles are going on right now.</p>';
-			} else {
-				buf = '<p>' + i + (i === 100 ? '+' : '') + ' ' + Tools.escapeFormat(this.format) + ' ' + (i === 1 ? 'battle' : 'battles') + '</p>' + buf;
-			}
-
-			this.$list.html(buf);
+			if (!buf.length) return this.$list.html('<p>No ' + Tools.escapeFormat(this.format) + ' battles are going on right now.</p>');
+			return this.$list.html('<p>' + buf.length + (buf.length === 100 ? '+' : '') + ' ' + Tools.escapeFormat(this.format) + ' ' + (buf.length === 1 ? 'battle' : 'battles') + '</p>' + buf.join(""));
 		},
 		refresh: function (i, button) {
-			button.disabled = true;
 			app.send('/cmd roomlist ' + this.format);
+
+			// Prevent further refreshes until we get a response.
+			button.disabled = true;
 		}
 	});
 
