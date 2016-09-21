657bd49bd1500fa03f6aed881b4b8f9de0cf479b
CamiloTD <camilotd1999@gmail.com>
2016-09-21 13:01:09 -0500

    Fix little code issues
    'npm test' Issues fixed

--- js/client-formatmods.js
+++ js/client-formatmods.js
@@ -1,49 +1,53 @@
-(function(exports, $){
+(function (exports, $) {
+
 	var FormatMods = exports.FormatMods = ({
-		tiershift: function tierShiftMod () {
+		tiershift: function tierShiftMod() {
 			var mod = {};
 
 			var keys = Object.keys(BattlePokedex);
 
-			for(var i=0, key, poke;key = keys[i++];){
+			for (var i = 0, key, poke; key < keys.length;) {
+				key = keys[i++];
 				poke = $.extend(true, {}, BattlePokedex[key]);
 
-				switch(poke.tier){
-					case "UU":
-					case "BL2": FormatMods._IncreaseStats(poke, 5); break;
+				switch (poke.tier) {
 
-					case "RU":
-					case "BL3": FormatMods._IncreaseStats(poke, 10); break;
+				case "UU":
+				case "BL2": FormatMods._IncreaseStats(poke, 5); break;
 
-					case "NU":
-					case "BL4": FormatMods._IncreaseStats(poke, 15); break;
+				case "RU":
+				case "BL3": FormatMods._IncreaseStats(poke, 10); break;
 
-					case "PU":
-					case "NFE":
-					case "LC Uber":
-					case "LC": FormatMods._IncreaseStats(poke, 20); break;
+				case "NU":
+				case "BL4": FormatMods._IncreaseStats(poke, 15); break;
+
+				case "PU":
+				case "NFE":
+				case "LC Uber":
+				case "LC": FormatMods._IncreaseStats(poke, 20); break;
+
 				}
 
-				mod[key] = poke
+				mod[key] = poke;
 			}
 
 			return mod;
 		}
 	});
 
-	FormatMods._IncreaseStats = function _SumStat (poke, quant) {
+	FormatMods._IncreaseStats = function _SumStat(poke, quant) {
 		var stats = poke.baseStats;
 
-		stats.atk = Math.clamp(stats.atk + quant, 1, 255);
-		stats.def = Math.clamp(stats.def + quant, 1, 255);
-		stats.hp  = Math.clamp(stats.hp  + quant, 1, 255);
-		stats.spa = Math.clamp(stats.spa + quant, 1, 255);
-		stats.spd = Math.clamp(stats.spd + quant, 1, 255);
-		stats.spe = Math.clamp(stats.spe + quant, 1, 255);
-	}
+		stats.atk = clamp(stats.atk + quant, 1, 255);
+		stats.def = clamp(stats.def + quant, 1, 255);
+		stats.hp = clamp(stats.hp + quant, 1, 255);
+		stats.spa = clamp(stats.spa + quant, 1, 255);
+		stats.spd = clamp(stats.spd + quant, 1, 255);
+		stats.spe = clamp(stats.spe + quant, 1, 255);
+	};
 
-	Math.clamp = function clamp(num, min, max){
-		return num < min? min : num > max? max : num;
+	function clamp(num, min, max) {
+		return num < min ? min : num > max ? max : num;
 	}
 
 })(window, jQuery);
