94500472602c38cab79ec9487f3d66630c037ec2
Marty-D <Marty-D@users.noreply.github.com>
2017-04-02 10:39:08 -0400

    Teambuilder: Set CAP LC to level 5

--- js/client-teambuilder.js
+++ js/client-teambuilder.js
@@ -2784,7 +2784,7 @@
 				if (baseFormat.substr(0, 8) === 'pokebank') baseFormat = baseFormat.substr(8);
 				if (this.curTeam && this.curTeam.format) {
 					if (baseFormat.substr(0, 10) === 'battlespot' || baseFormat.substr(0, 3) === 'vgc') set.level = 50;
-					if (baseFormat.substr(0, 2) === 'lc') set.level = 5;
+					if (baseFormat.substr(0, 2) === 'lc' || baseFormat.substr(0, 5) === 'caplc') set.level = 5;
 				}
 			}
 			if (set.gender) delete set.gender;
