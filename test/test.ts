1c2f5b0729b100d7d01a2b27ea081a79796772a2
Konrad Borowski <konrad@borowski.pw>
2017-07-06 08:34:20 +0200

    Add level 50 Pokemon test for VGC (#964)

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,19 @@
 import { Selector } from 'testcafe';
+import { execFile } from 'child_process';
+
+const showdownDirectory = __dirname + '/../data/Pokemon-Showdown';
 
 fixture('Pokemon Showdown')
-    .page('../testclient.html?~~localhost');
+    .page('../testclient.html?~~localhost')
+    .before(async ctx => {
+        ctx.process = execFile(showdownDirectory + '/pokemon-showdown', {
+            cwd: showdownDirectory,
+        });
+    })
+    .after(async ctx => {
+        ctx.process.kill();
+    })
+    .beforeEach(t => t.maximizeWindow());
 
 const expectedTeam = `=== Untitled 1 ===
 
@@ -19,4 +31,16 @@ test('Teambuilder works', async t => {
         .click('[name="backup"]');
 
     await t.expect((await Selector('textarea').value).trim()).eql(expectedTeam);
+});
+
+test('Teambuilder shows level 50 for VGC', async t => {
+    await t.click('[name="close"]')
+        .click('[value="teambuilder"]')
+        .click('[name="newTop"]')
+        .click('.teambuilderformatselect')
+        .click('[value^="gen7vgc"]')
+        .click('[name="addPokemon"]')
+        .click('[data-entry="pokemon|Absol"]')
+        .click('.setdetails')
+        .expect(Selector('[name="level"]').value).eql('50');
 });
