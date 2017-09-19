ced962bffd6a91ff20809b2dd3644e009fc1d008
MacChaeger <msaimrkon@gmail.com>
2017-09-19 16:10:38 -0500

    Remove redundant item name

--- js/client-battle-tooltips.js
+++ js/client-battle-tooltips.js
@@ -1289,7 +1289,6 @@ var BattleTooltips = (function () {
 		'SilverPowder': 'Bug',
 		'Soft Sand': 'Ground',
 		'Spell Tag': 'Ghost',
-		'TwistedSpoon': 'Psychic'
 		'Twisted Spoon': 'Psychic'
 	};
 	var orbUsers = {
