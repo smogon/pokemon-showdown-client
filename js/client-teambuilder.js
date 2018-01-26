f00f9adc662dee03c69fb7f816d799bec1c94022
Guangcong Luo <guangcongluo@gmail.com>
2018-01-26 15:18:45 -0500

    Teambuilder: Remove Team Recovery Tool link
    The Team Recovery Tool still exists, but it's not linked from the
    teambuilder anymore because it's only intended to be a Safari HTTPS
    migration tool, and will not actually help the majority of cases of
    teams being lost.

--- js/client-teambuilder.js
+++ js/client-teambuilder.js
@@ -322,9 +322,6 @@
 				buf += '<h2>Hi</h2>';
 				buf += '<p>Did you have a good day?</p>';
 				buf += '<p><button class="button" name="greeting" value="Y"><i class="fa fa-smile-o"></i> Yes, my day was pretty good</button> <button class="button" name="greeting" value="N"><i class="fa fa-frown-o"></i> No, it wasn\'t great</button></p>';
-				if (Storage.teams && !Storage.teams.length) {
-					buf += '<p><a style="color:#AA2222;text-decoration:none" href="http://play.pokemonshowdown.com/recoverteams.html" target="blank">Some people have reported losing their teams in our switch to HTTPS. If that\'s you, use this <u><b>Team Recovery Tool</b></u> to get your teams back.</a></p>';
-				}
 				buf += '<h2>All teams</h2>';
 			} else {
 				if (this.curFolder.slice(-1) === '/') {
