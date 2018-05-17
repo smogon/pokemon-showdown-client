8df2e521e3351c3fa14b951cb488f6ca577bbc31
Guangcong Luo <guangcongluo@gmail.com>
2018-05-17 11:43:15 -0500

    Fix build tools
    It should now be possible to build from a fresh checkout with
    `node build`.

---
+++ config/config-example.js
@@ -0,0 +1,125 @@
+var Config = Config || {};
+
+/* version */ Config.version = "0";
+
+Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
+
+Config.whitelist = [
+	// general sites
+	'wikipedia\\.org',
+	'wikimedia\\.org',
+	'wiktionary\\.org',
+	'github\\.com',
+	'reddit\\.com',
+	'gamefaqs\\.com',
+	'facebook\\.com',
+	'fbcdn\\.net',
+	'twitter\\.com',
+	'deviantart\\.com',
+	'youtube\\.com',
+	'youtu\\.be',
+	'zombo\\.com',
+	'strawpoll\\.me',
+	'twitch\\.tv',
+	'take-a-screenshot\\.org',
+	'myanimelist\\.net',
+	'4chan\\.org',
+	'tumblr\\.com',
+	'git\\.io',
+	'mibbit\\.com',
+	'codecademy\\.com',
+	'xkcd\\.com',
+	'stackoverflow\\.com',
+	'stackexchange\\.com',
+	'malwarebytes\\.org',
+	'animenewsnetwork\\.com',
+	'animenewsnetwork\\.cc',
+	'zombo\\.com',
+	'html5zombo\\.com',
+
+	// pokemon sites
+	'pokemonshowdown\\.com',
+	'psim\\.us',
+	'smogon\\.com',
+	'upokecenter\\.com',
+	'veekun\\.com',
+	'bulbagarden\\.net',
+	'serebii\\.net',
+	'nuggetbridge\\.com',
+	'pokecommunity\\.com',
+	'pokemon-online\\.eu',
+	'pokemonlab\\.com',
+	'shoddybattle\\.com',
+	'pokemonxy\\.com',
+	'pokemon\\.com',
+	'pokemon-gl\\.com',
+	'pokecheck\\.org',
+	'projectpokemon\\.org',
+	'pokemondb\\.net',
+	'pokemoncentral\\.it',
+	'poketrade\\.us',
+	'neverused\\.net',
+	'pokestrat\\.com',
+	'spo\\.ink',
+	'jooas\\.com',
+	'pokemongodb\\.net',
+	'pokeassistant\\.com',
+	'pokemon-sunmoon\\.com',
+	'gamepress\\.gg',
+	'trainertower\\.com',
+	'pokepast\\.es',
+
+	// personal sites
+	'breakdown\\.forumotion\\.com',
+	'pokemonmillennium\\.net',
+	'thebattletower\\.no-ip\\.org',
+	'meltsner\\.com',
+	'guangcongluo\\.com',
+	'cathyjf\\.com',
+	'xiaotai\\.org',
+	'xfix\\.pw',
+	'bumba\\.me',
+	'strategydatabase\\.jimdo\\.com',
+	'hidden50\\.github\\.io',
+	// personal hosting sites
+	'forumieren\\.com',
+	'soforums\\.com',
+	'proboards\\.com',
+	'weebly\\.com',
+	'freeforums\\.org',
+	'forumactif\\.com',
+	'forumotion\\.com',
+	'bigbangpokemon\\.com',
+
+	// rich text
+	'docs\\.google\\.com',
+
+	// text
+	'pastebin\\.com',
+	'hastebin\\.com',
+	'trello\\.com',
+	'challonge\\.com',
+	'piratepad\\.net',
+
+	// music
+	'plug\\.dj',
+	'openings\\.moe',
+
+	// images
+	'prntscr\\.com',
+	'prnt\\.sc',
+	'puu\\.sh',
+	'd\\.pr',
+	'snag\\.gy',
+	'gyazo\\.com',
+	'imgur\\.com',
+	'gfycat\\.com',
+	'4cdn\\.org'
+];
+
+Config.roomsFirstOpenScript = function () {
+};
+
+Config.customcolors = {
+	'zarel': 'aeo'
+};
