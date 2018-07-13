ed1abf0530592c365bfed308e7dafb9ebd8c2ce8
buck20two <buck.20.two@gmail.com>
2018-07-13 16:30:27 -0600

    add search to team builder

--- js/client-teambuilder.js
+++ js/client-teambuilder.js
@@ -2586,7 +2586,7 @@
 		},
 		searchChange: function (e) {
 			//91 for right CMD / 93 for left / CMD 17 for CTL
-			if (e.keyCode != 91 && e.keyCode != 93 && e.keyCode != 17) {
+			if (e.keyCode !== 91 && e.keyCode !== 93 && e.keyCode !== 17) {
 				this.curSearchVal = e.currentTarget.value;
 				this.updateTeamList();
 			}
