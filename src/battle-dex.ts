5f8bb7a92e7b9ea9a154175d8e63afa177080bf6
Guangcong Luo <guangcongluo@gmail.com>
2018-10-28 05:31:15 -0500

    Suppress LGTM warnings
    Neither of these warnings are actually incorrect sanitization, but
    unfortunately there's no way for any automated tool to know that.

--- src/battle-dex.ts
+++ src/battle-dex.ts
@@ -261,7 +261,7 @@ const Tools = {
 			// custom avatar served by the server
 			let protocol = (Config.server.port === 443) ? 'https' : 'http';
 			return protocol + '://' + Config.server.host + ':' + Config.server.port +
-				'/avatars/' + encodeURIComponent(avatar).replace('%3F', '?');
+				'/avatars/' + encodeURIComponent(avatar).replace(/\%3F/g, '?');
 		}
 		// just pick a random avatar
 		let sprites = [1, 2, 101, 102, 169, 170];
@@ -1600,7 +1600,7 @@ const Tools = {
 		buf += '<input type="hidden" name="replayid" value="' + replayid + '" />\n';
 		buf += '<div class="battle"></div><div class="battle-log"></div><div class="replay-controls"></div><div class="replay-controls-2"></div>\n';
 		buf += '<h1 style="font-weight:normal;text-align:center"><strong>' + Tools.escapeHTML(battle.tier) + '</strong><br /><a href="http://pokemonshowdown.com/users/' + toId(battle.p1.name) + '" class="subtle" target="_blank">' + Tools.escapeHTML(battle.p1.name) + '</a> vs. <a href="http://pokemonshowdown.com/users/' + toId(battle.p2.name) + '" class="subtle" target="_blank">' + Tools.escapeHTML(battle.p2.name) + '</a></h1>\n';
-		buf += '<script type="text/plain" class="battle-log-data">' + battle.activityQueue.join('\n').replace(/\//g, '\\/') + '</script>\n';
+		buf += '<script type="text/plain" class="battle-log-data">' + battle.activityQueue.join('\n').replace(/\//g, '\\/') + '</script>\n'; // lgtm [js/incomplete-sanitization]
 		buf += '</div>\n';
 		buf += '<div class="battle-log battle-log-inline"><div class="inner">' + battle.scene.$log.html() + '</div></div>\n';
 		buf += '</div>\n';
