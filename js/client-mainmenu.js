f2f81ac374b01dee15299c85a8fd1f392fab5627
Guangcong Luo <guangcongluo@gmail.com>
2018-11-19 16:18:15 -0600

    Remove "Gen 7" from Let's Go formats in list
    So "[Gen 7 Let's Go]" becomes "[Let's Go]"

--- js/client-mainmenu.js
+++ js/client-mainmenu.js
@@ -1042,6 +1042,7 @@
 				var formatName = BattleLog.escapeFormat(format.id);
 				if (formatName.charAt(0) !== '[') formatName = '[Gen 6] ' + formatName;
 				formatName = formatName.replace('[Gen 7] ', '');
+				formatName = formatName.replace('[Gen 7 ', '[');
 				bufs[curBuf] += '<li><button name="selectFormat" value="' + i + '"' + (curFormat === i ? ' class="sel"' : '') + '>' + formatName + '</button></li>';
 			}
 
