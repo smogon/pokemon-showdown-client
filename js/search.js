e62302417ce042af47de99ce2130857d7f78c168
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2019-01-22 18:00:12 -0700

    Support Untiered as a Doubles tier in teambuilder (#1216)

--- js/search.js
+++ js/search.js
@@ -854,7 +854,7 @@
 			else if (format === 'balancedhackmons' || format === 'bh') tierSet = agTierSet.concat(tierSet.slice(slices.Uber));
 			else if (format === 'doublesou') tierSet = tierSet.slice(slices.DOU);
 			else if (format === 'doublesuu') tierSet = tierSet.slice(slices.DUU);
-			else if (format === 'doublesnu') tierSet = tierSet.slice(slices.DNU);
+			else if (format === 'doublesnu') tierSet = tierSet.slice(slices.Untiered || slices.DUU);
 			else if (isLetsGo) tierSet = tierSet.slice(slices.Uber);
 			// else if (isDoublesOrBS) tierSet = tierSet;
 			else if (!isDoublesOrBS) tierSet = tierSet.slice(slices.OU, slices.UU).concat(agTierSet).concat(tierSet.slice(slices.Uber, slices.OU)).concat(tierSet.slice(slices.UU));
