0e907cb9a334c7777d40548cce82ff48df09afd2
Alexander B <mathfreak231@users.noreply.github.com>
2019-02-07 11:12:01 -0600

    Fix PP in tooltips (Trump Card, Pressure) (#1223)

--- js/client-battle-tooltips.js
+++ js/client-battle-tooltips.js
@@ -820,10 +820,10 @@ var BattleTooltips = (function () {
 				var table = BattleTeambuilderTable['gen' + this.battle.gen];
 				if (move.id in table.overridePP) maxpp = table.overridePP[move.id];
 			}
-			maxpp = Math.floor(maxpp * 8 / 5);
+			if (!move.noPPBoosts) maxpp = Math.floor(maxpp * 8 / 5);
 		}
 		if (ppUsed === Infinity) return move.name + ' <small>(0/' + maxpp + ')</small>';
-		if (ppUsed || moveName.charAt(0) === '*') return move.name + ' <small>(' + (maxpp - ppUsed) + '/' + maxpp + ')</small>';
+		if (ppUsed || moveName.charAt(0) === '*') return move.name + ' <small>(' + Math.max(maxpp - ppUsed, 0) + '/' + maxpp + ')</small>';
 		return move.name + (showKnown ? ' <small>(revealed)</small>' : '');
 	};
 
