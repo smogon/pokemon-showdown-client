d2ef03c07afa881a4345fd82b72dd04fed344c13
Guangcong Luo <guangcongluo@gmail.com>
2019-02-20 03:09:59 -0500

    Fix tooltips in downloaded replays

--- js/replay-embed.js
+++ js/replay-embed.js
@@ -25,11 +25,11 @@ requireScript('https://play.pokemonshowdown.com/js/battledata.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/pokedex-mini.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/pokedex-mini-bw.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/graphics.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/pokedex.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/moves.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/abilities.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/items.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/teambuilder-tables.js?a7');
+requireScript('https://play.pokemonshowdown.com/data/pokedex.js?a7');
+requireScript('https://play.pokemonshowdown.com/data/moves.js?a7');
+requireScript('https://play.pokemonshowdown.com/data/abilities.js?a7');
+requireScript('https://play.pokemonshowdown.com/data/items.js?a7');
+requireScript('https://play.pokemonshowdown.com/data/teambuilder-tables.js?a7');
 requireScript('https://play.pokemonshowdown.com/js/battle-tooltips.js?a7');
 requireScript('https://play.pokemonshowdown.com/js/battle.js?a7');
 
