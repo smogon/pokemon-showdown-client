ec872daa1ab5ddaecfc6a3518f44fa742f440c7c
Kirk Scheibelhut <kjs@google.com>
2019-05-28 10:51:59 -0700

    Remove Config.customcolors 'color' options

--- src/battle-log.ts
+++ src/battle-log.ts
@@ -381,9 +381,6 @@ class BattleLog {
 		if (this.colorCache[name]) return this.colorCache[name];
 		let hash;
 		if (window.Config && Config.customcolors && Config.customcolors[name]) {
-			if (Config.customcolors[name].color) {
-				return (this.colorCache[name] = Config.customcolors[name].color);
-			}
 			hash = MD5(Config.customcolors[name]);
 		} else {
 			hash = MD5(name);
