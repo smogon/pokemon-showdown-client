0f288bf75fcdfcb3cf53cddd4bcd61bfcc2d92a6
TheJetOU <waleed.2004.hassan@gmail.com>
2019-06-12 18:39:36 +0300

    Fix bug in hiding tournaments

--- js/client-chat-tournament.js
+++ js/client-chat-tournament.js
@@ -245,6 +245,8 @@
 		};
 
 		TournamentBox.prototype.parseMessage = function (data, isBroadcast) {
+			var hide = Dex.prefs('tournaments') === 'hide';
+			if (hide) return;
 			var notify = Dex.prefs('tournaments') === 'notify';
 			var cmd = data.shift().toLowerCase();
 			if (isBroadcast) {
