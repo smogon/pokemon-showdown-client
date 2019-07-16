06e6e264db564041ae631c835d680f7ab39dc548
Guangcong Luo <guangcongluo@gmail.com>
2019-07-16 17:08:21 -0400

    Support Blitz custom rule

--- src/battle.ts
+++ src/battle.ts
@@ -3106,6 +3106,7 @@ class Battle {
 		case 'rule': {
 			let ruleName = args[1].split(': ')[0];
 			if (ruleName === 'Species Clause') this.speciesClause = true;
+			if (ruleName === 'Blitz') this.isBlitz = true;
 			this.log(args);
 			break;
 		}
