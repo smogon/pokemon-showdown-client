6c418a70316dfd500390e85eb88cfcf836a9b40d
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-01-23 05:03:15 -0700

    Teambuilder: Unhardcode inherited learnsets (#1451)

--- src/battle-dex-data.ts
+++ src/battle-dex-data.ts
@@ -1339,6 +1339,7 @@ class Template implements Effect {
 	readonly battleOnly: boolean;
 	readonly isNonstandard: string | null;
 	readonly unreleasedHidden: boolean;
+	readonly inheritsFrom: string | null;
 
 	constructor(id: ID, name: string, data: any) {
 		if (!data || typeof data !== 'object') data = {};
@@ -1419,6 +1420,7 @@ class Template implements Effect {
 		this.battleOnly = !!data.battleOnly;
 		this.isNonstandard = data.isNonstandard || null;
 		this.unreleasedHidden = !!data.unreleasedHidden;
+		this.inheritsFrom = (Array.isArray(data.inheritsFrom) ? this.baseSpecies : data.inheritsFrom) || null;
 		if (!this.gen) {
 			if (this.num >= 810 || this.forme === 'Galar' || this.isGigantamax) {
 				this.gen = 8;
