0cd6a71962f81a0e74b7c982e39512e168817aa8
The Immortal <the_immortal123@live.com>
2020-01-28 13:28:41 +0400

    Update `eatItem` text

--- data/text.js
+++ data/text.js
@@ -58,7 +58,7 @@ exports.BattleText = {
 		changeAbility: "  [POKEMON] acquired [ABILITY]!",
 		addItem: "  [POKEMON] obtained one [ITEM].", // Trick, Switcheroo
 		takeItem: "  [POKEMON] stole [SOURCE]'s [ITEM]!", // Thief, Covet, Magician, Pickpocket
-		eatItem: "  [POKEMON] ate its [ITEM]!",
+		eatItem: "  ([POKEMON] ate its [ITEM]!)",
 		useGem: "  The [ITEM] strengthened [POKEMON]'s power!",
 		eatItemWeaken: "  The [ITEM] weakened damage to [POKEMON]!",
 		removeItem: "  [POKEMON] lost its [ITEM]!",
