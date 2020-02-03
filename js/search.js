94fcf20ea0630cc0711a8644c5a77a749e49404a
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-02-03 02:41:33 -0700

    Remove useless comment

--- js/search.js
+++ js/search.js
@@ -808,7 +808,6 @@
 			if (!format) format = 'ou';
 		}
 		var requirePentagon = (format === 'battlespotsingles' || format === 'battledoubles' || format.slice(0, 3) === 'vgc');
-		 // CAP check is temporary
 		var requireGalar = (format.indexOf('battlestadium') >= 0 || (format.slice(0, 3) === 'vgc' && this.gen === 8));
 		var template;
 		var isBH = (format === 'balancedhackmons' || format === 'bh');
