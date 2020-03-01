07d93e08669d5a0c01aaffbf53cb6eda20d549cf
Kirk Scheibelhut <kjs@scheibo.com>
2020-03-01 14:06:31 -0800

    Add missing gen 8 support to AltFormPopup

--- js/client-teambuilder.js
+++ js/client-teambuilder.js
@@ -3181,7 +3181,7 @@
 			var spriteSize = 96;
 			var spriteDim = 'width: 96px; height: 96px;';
 
-			var gen = {1:'gen1', 2:'gen2', 3:'gen3', 4:'gen4', 5:'gen5', 6:'dex', 7:'dex'}[Math.max(this.room.curTeam.gen, template.gen)];
+			var gen = {1:'gen1', 2:'gen2', 3:'gen3', 4:'gen4', 5:'gen5', 6:'dex', 7:'dex', 8:'dex'}[Math.max(this.room.curTeam.gen, template.gen)];
 			if (Dex.prefs('nopastgens')) gen = 'dex';
 			if (Dex.prefs('bwgfx') && gen === 'dex') gen = 'gen5';
 			spriteDir += gen;
