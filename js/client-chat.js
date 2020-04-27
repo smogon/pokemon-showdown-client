3ad34f8d3832f63b936a6fbd611aa9519b95ece6
Mia <49593536+mia-pi-git@users.noreply.github.com>
2020-04-27 12:58:17 -0500

    Update client-chat.js

--- js/client-chat.js
+++ js/client-chat.js
@@ -562,7 +562,6 @@
 
 			case 'news':
 				var newsId = '1990';
-				if (newsId === '' + Dex.prefs('readnews')) return;
 				app.rooms[''].addPseudoPM({
 					title: 'News',
 					html: '<iframe src="/news-embed.php?news' + (window.nodewebkit || document.location.protocol === 'https:' ? '&amp;https' : '') + '" width="270" height="400" border="0" style="border:0;width:100%;height:100%;display:block"></iframe>',
