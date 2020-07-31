e41b726a5b6df97d0bec1b0a0bdaf5c58211141b
Kirk Scheibelhut <kjs@scheibo.com>
2020-07-30 20:13:15 -0700

    Fix eevee's name

--- website/credits.php
+++ website/credits.php
@@ -98,7 +98,7 @@ includeHeader();
 				<li><p><a href="http://smogon.com/forums/threads/3486712/" target="_blank" class="subtle"><strong>X/Y</strong></a> and <a href="http://www.smogon.com/forums/threads/3577711/" target="_blank" class="subtle"><strong>Sun/Moon Sprite Projects</strong></a> led by <strong>Ian Clail</strong> [Layell]<small>&ndash; Sprites</small></p></li>
 				<li><p><a href="http://www.smogon.com/forums/threads/3577711/" target="_blank" class="subtle"><strong>Sun/Moon</strong></a> and <a href="https://www.smogon.com/forums/threads/3647722/" target="_blank" class="subtle"><strong>Sword/Shield Sprite Projects</strong></a> led by [<strong>leparagon</strong>] <small>&ndash; Sprites</small></p></li>
 				<li><p><a href="http://www.pkparaiso.com/xy/sprites_pokemon.php" target="_blank" class="subtle"><strong>PKPara&iacute;so</strong></a> <small>&ndash; Gen 6, 7, and 8 sprite ripping</small></p></li>
-				<li><p><a href="http://veekun.com/" target="_blank" class="subtle"><strong>Veekun</strong></a> led by <strong>Alex Munroe</strong> [eevee] <small>&ndash; Gen 5 data</small></p></li>
+				<li><p><a href="http://veekun.com/" target="_blank" class="subtle"><strong>Veekun</strong></a> led by <strong>Evelyn Woods</strong> [eevee] <small>&ndash; Gen 5 data</small></p></li>
 			</ul>
 
 		</div>
