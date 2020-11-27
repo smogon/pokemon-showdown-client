3747f2eb929d7935bfd02f832ceb212aaa123954
SirKira(!Uzumaki) <48526201+sirKiraUzumaki@users.noreply.github.com>
2020-11-27 12:32:50 +0530

    Update src/battle-log.ts
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- src/battle-log.ts
+++ src/battle-log.ts
@@ -810,8 +810,8 @@ class BattleLog {
 				const src = getAttrib('src');
 				const songId = /(?:\?v=|\/track\/)([A-Za-z0-9]+)/.exec(src)[1];
 						
-            return {
-				   tagName: 'iframe',
+				return {
+					tagName: 'iframe',
 					attribs: ['src', `https://open.spotify.com/embed/track/${songId}`,'width', '300', 'height', '380', 'frameborder', '0', 'allowtransparency', 'true', 'allow', 'encrypted-media'],
 				};
 		   } else if (tagName === 'youtube') {
