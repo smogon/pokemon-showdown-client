329c9682acbde3718b0b755f2b5ca485b960754f
SirKira(!Uzumaki) <48526201+sirKiraUzumaki@users.noreply.github.com>
2020-11-29 16:46:36 +0530

    Update battle-log.ts

--- src/battle-log.ts
+++ src/battle-log.ts
@@ -809,7 +809,7 @@ class BattleLog {
 				// <iframe src="https://open.spotify.com/embed/track/6aSYnCIwcLpnDXngGKAEzZ" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 				const src = getAttrib('src') || '';
 				const songId = /(?:\?v=|\/track\/)([A-Za-z0-9]+)/.exec(src)?.[1];
-				
+
 				return {
 					tagName: 'iframe',
 					attribs: ['src', `https://open.spotify.com/embed/track/${songId}`, 'width', '300', 'height', '380', 'frameborder', '0', 'allowtransparency', 'true', 'allow', 'encrypted-media'],
