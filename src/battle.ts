390c387f60631eec6bea8b22cbd1080bc4aa044d
Karthik99999 <bandagondak0217@gmail.com>
2021-05-02 13:27:43 -0400

    add updatepokemon protocol

--- src/battle.ts
+++ src/battle.ts
@@ -3292,6 +3292,18 @@ class Battle {
 			}
 			break;
 		}
+		case 'updatepokemon': {
+			const {siden} = this.parsePokemonId(args[1]);
+			const side = this.sides[siden]
+			for (var i = 0; i < side.pokemon.length; i++) {
+				const pokemon = side.pokemon[i];
+				if (pokemon.details === args[2]) {
+					side.addPokemon('', '', args[3], i)
+					break;
+				}
+			}
+			break;
+		}
 		case 'teampreview': {
 			this.teamPreviewCount = parseInt(args[1], 10);
 			this.scene.teamPreview();
