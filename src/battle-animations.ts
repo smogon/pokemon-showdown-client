f5500c5b960072855029425da770f1c173984bd3
Tristan Pink <tristanpink22@gmail.com>
2021-05-06 08:46:42 -0500

    Add null effect to furycutter volatile

--- src/battle-animations.ts
+++ src/battle-animations.ts
@@ -1804,8 +1804,7 @@ class PokemonSprite extends Sprite {
 		laserfocus: ['Laser Focus', 'good'],
 		spotlight: ['Spotlight', 'neutral'],
 		itemremoved: null,
-		furycutter1: null,
-		furycutter2: null,
+		furycutter: null,
 		// partial trapping
 		bind: ['Bind', 'bad'],
 		clamp: ['Clamp', 'bad'],
