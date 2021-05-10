7026a305401e6acc11a1e06c73221ffb5b9eb3fa
Tristan Pink <tristanpink22@gmail.com>
2021-05-09 20:35:20 -0500

    Fix tooltip and scene to display correct BP

--- src/battle-tooltips.ts
+++ src/battle-tooltips.ts
@@ -927,6 +927,10 @@ class BattleTooltips {
 		}
 		buf += `</tr><table>`;
 		if (!atLeastOne) buf = ``;
+					 
+		if (this.battle.echoedVoiceMultiplier > 1) {
+			buf = `<p>Echoed Voice: ${40 * this.battle.echoedVoiceMultiplier} BP</p>` + buf;
+		}
 
 		let weatherbuf = scene.weatherLeft() || `(no weather)`;
 		if (weatherbuf.startsWith('<br />')) {
@@ -1498,6 +1502,9 @@ class BattleTooltips {
 		}
 		if (move.id === 'brine' && target && target.hp * 2 <= target.maxhp) {
 			value.modify(2, 'Brine + target below half HP');
+		}
+		if (move.id === 'echoedvoice' && this.battle.echoedVoiceMultiplier > 1) {
+			value.modify(this.battle.echoedVoiceMultiplier, 'Echoed Voice used in succession');
 		}
 		if (move.id === 'eruption' || move.id === 'waterspout' || move.id === 'dragonenergy') {
 			value.set(Math.floor(150 * pokemon.hp / pokemon.maxhp) || 1);
