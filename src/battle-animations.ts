28fc43f76c67af5ab6938f2e421fbe0ec5d57f7b
not-the-fiend <78666508+not-the-fiend@users.noreply.github.com>
2021-06-05 20:09:51 -0400

    gives livewire a thingamathing
    yeah!

--- src/battle-animations.ts
+++ src/battle-animations.ts
@@ -1761,6 +1761,7 @@ class PokemonSprite extends Sprite {
 		mudsport: ['Mud Sport', 'good'],
 		waitforit: ['Wait For It', 'neutral'],
 		substitute: null,
+		livewire: ['Livewire', 'bad'],
 		// sub graphics are handled elsewhere, see Battle.Sprite.animSub()
 		uproar: ['Uproar', 'neutral'],
 		rage: ['Rage', 'neutral'],
