c23e1f4aea93b700a0e945ccc628723a114e5035
Karthik99999 <bandagondak0217@gmail.com>
2021-06-26 19:12:16 +0530

    Change dex to let's go when |tier is sent

--- src/battle.ts
+++ src/battle.ts
@@ -3201,6 +3201,9 @@ class Battle {
 				this.messageFadeTime = 40;
 				this.isBlitz = true;
 			}
+			if (this.tier.includes(`Let's Go`)) {
+				this.dex = Dex.mod('letsgo' as ID, 7);
+			}
 			this.log(args);
 			break;
 		}
