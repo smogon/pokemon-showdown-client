756779b207a0a6fbe107fdc6eb844cd3858abedb
Guangcong Luo <guangcongluo@gmail.com>
2021-07-05 14:46:26 -0400

    Reject invalid email addresses for 2FA settings
    This isn't a real validation, it's just to guard against typoes.

--- website/users.php
+++ website/users.php
@@ -266,21 +266,29 @@ if (!$user) {
 		} else if ($csrfOk && isset($_POST['googlelogin'])) {
 			$email = $_POST['googlelogin'];
 			$remove = ($email === 'remove');
-			$psdb->query(
-				"UPDATE {$psdb->prefix}users SET email = ? WHERE userid = ?",
-				[$remove ? '' : $email . '@', $user['userid']]
-			);
+			if (!$remove && strpos($email, '@') === false || strpos($email, '.') === false) {
+?>
+				<div style="border: 1px solid #AADD88; padding: 0 1em; margin-bottom: 1em">
+					<p>Invalid e-mail address "<?= htmlspecialchars($email) ?>"</p>
+				</div>
+<?php
+			} else {
+				$psdb->query(
+					"UPDATE {$psdb->prefix}users SET email = ? WHERE userid = ?",
+					[$remove ? '' : $email . '@', $user['userid']]
+				);
 
-			$modlogentry = $remove ? "Login method set to password" : "Login method set to Google " . $email;
-			$psdb->query(
-				"INSERT INTO `{$psdb->prefix}usermodlog` (`userid`,`actorid`,`date`,`ip`,`entry`) VALUES (?, ?, ?, ?, ?)",
-				[$user['userid'], $curuser['userid'], time(), $users->getIp(), $modlogentry]
-			);
+				$modlogentry = $remove ? "Login method set to password" : "Login method set to Google " . $email;
+				$psdb->query(
+					"INSERT INTO `{$psdb->prefix}usermodlog` (`userid`,`actorid`,`date`,`ip`,`entry`) VALUES (?, ?, ?, ?, ?)",
+					[$user['userid'], $curuser['userid'], time(), $users->getIp(), $modlogentry]
+				);
 ?>
 		<div style="border: 1px solid #DDAA88; padding: 0 1em; margin-bottom: 1em">
 			<p>Login method updated</p>
 		</div>
 <?php
+			}
 		} else if ($csrfOk && $authLevel >= 6 && @$_POST['passreset']) {
 			$token = $users->createPasswordResetToken($user['userid']);
 ?>
