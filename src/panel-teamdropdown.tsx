94687f500282b53def0891982581a2ac3f94c408
Guangcong Luo <guangcongluo@gmail.com>
2021-11-18 22:31:18 -0500

    Update for latest TypeScript
    This seems to be a clear TypeScript bug, but oh well, the workaround
    is likely to be better code anyway.

--- src/panel-teamdropdown.tsx
+++ src/panel-teamdropdown.tsx
@@ -790,7 +790,7 @@ class FormatDropdownPanel extends PSRoomPanel {
 
 		let curSection = '';
 		let curColumnNum = 0;
-		let curColumn: (FormatData | {id: '', section: string})[] = [];
+		let curColumn: (FormatData | {id: null, section: string})[] = [];
 		const columns = [curColumn];
 		for (const format of formats) {
 			if (format.column !== curColumnNum) {
@@ -803,7 +803,7 @@ class FormatDropdownPanel extends PSRoomPanel {
 			if (format.section !== curSection) {
 				curSection = format.section;
 				if (curSection) {
-					curColumn.push({id: '', section: curSection});
+					curColumn.push({id: null, section: curSection});
 				}
 			}
 			curColumn.push(format);
