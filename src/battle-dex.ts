b14c30250e1ee10b86e1f673f8eb393f5caa1feb
Aegide <geoffroygressier1@yahoo.fr>
2021-11-20 14:39:03 +0100

    Update fxPrefix system

--- src/battle-dex.ts
+++ src/battle-dex.ts
@@ -186,11 +186,16 @@ const Dex = new class implements ModdedDex {
 	})();
 
 	fxPrefix = (() => {
+		/*
 		if (window.document?.location?.protocol === 'file:') {
 			if (window.Replays) return `https://${window.Config ? Config.routes.client : 'play.pokemonshowdown.com'}/fx/`;
 			return `fx/`;
 		}
 		return `//${window.Config ? Config.routes.client : 'play.pokemonshowdown.com'}/fx/`;
+		*/
+		let prefix = '';
+		if (window.document?.location?.protocol !== 'http:') prefix = 'https:';
+		return `${prefix}//${window.Config ? Config.routes.client : 'play.pokemonshowdown.com'}/fx/`;
 	})();
 
 	loadedSpriteData = {xy: 1, bw: 0};
