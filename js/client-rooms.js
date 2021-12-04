fb7c2ae2981a85fdd95e96a6045eb87d2d43e69c
missyaria <93721556+missyaria@users.noreply.github.com>
2021-12-04 07:49:53 +0100

    Add files via upload

--- js/client-rooms.js
+++ js/client-rooms.js
@@ -87,14 +87,16 @@
 			}
 			if (roomData.subRooms && roomData.subRooms.length) {
 				var buf = '<div class="ilink" style="border-bottom-left-radius:0px;border-bottom-right-radius:0px"><a style="color:inherit" href="' + app.root + id + '"><small style="float:right">(' + Number(roomData.userCount) + ' users)</small><strong><i class="fa fa-comment-o"></i> ' + BattleLog.escapeHTML(roomData.title) + '<br /></strong><small>' + BattleLog.escapeHTML(roomData.desc || '') + '</a>';
-				buf += '</div><div class="ilink" style="cursor:auto;margin:-5px 7px 4px 7px;border-top-left-radius:0px;border-top-right-radius:0px"><i class="fa fa-level-up fa-rotate-90"></i> Subrooms: <strong>';
 				for (var i = 0; i < roomData.subRooms.length; i++) {
-					if (i) buf += ', ';
-					buf += '<i class="fa fa-comment-o" ></i>' + ' <a class="subroom" style="display:inline" href="/' + roomData.subRooms[i].split(" ").join("").toLowerCase() + '">' + BattleLog.escapeHTML(roomData.subRooms[i]) + '</a>';
+					if (i==roomData.subRooms.length-1){
+						buf += '<strong></div><div class="ilink" style="cursor:auto;margin:-5px 7px 4px 7px;border-top-left-radius:0px;border-top-right-radius:0px"> ';
+					}
+					if (i!=roomData.subRooms.length-1)
+					buf += '</div><div class="ilink" style="cursor:auto;margin:-5px 7px 4px 7px;border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px">';
+					buf += ' <strong><a class="subroom"  href="/' + roomData.subRooms[i].split(" ").join("").toLowerCase() + '">' + '<i class="fa fa-comment-o" ></i> ' + BattleLog.escapeHTML(roomData.subRooms[i]) + '</a></strong></strong>';
 				}
-				buf += '</strong>';
 			}
-			buf += '</small></div>';
+			buf += '</strong></small></div>';
 			return buf;
 		},
 		compareRooms: function (roomA, roomB) {
