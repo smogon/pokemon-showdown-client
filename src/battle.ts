ee09fec7004e69c1a855da67dea07c4a810f09ae
Alexander B <4866817+MathyFurret@users.noreply.github.com>
2022-12-26 12:28:16 -0600

    Revert "Type is redundant"
    This reverts commit 244b5fdbbd31cdc263f7cd4d143b1b617a9b5f4b.

--- src/battle.ts
+++ src/battle.ts
@@ -2472,8 +2472,8 @@ export class Battle {
 			let poke = this.getPokemon(args[1])!;
 			let type = Dex.types.get(args[2]).name;
 			poke.terastallized = type;
-			poke.details += `, tera`;
-			poke.searchid += `, tera`;
+			poke.details += `, tera:${type}`;
+			poke.searchid += `, tera:${type}`;
 			this.scene.animTransform(poke, true, true);
 			this.log(args, kwArgs);
 			break;
