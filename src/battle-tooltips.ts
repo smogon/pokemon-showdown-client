f0598bb924cd2ca4a3fa68fcc7a9c54d2897dfa3
qwertystrargs <119243068+qwertystrargs@users.noreply.github.com>
2023-01-12 20:53:34 -0500

    Tooltips: Align Tera reminders with types (#2085)

--- src/battle-tooltips.ts
+++ src/battle-tooltips.ts
@@ -802,11 +802,11 @@ class BattleTooltips {
 			} else if (clientPokemon?.volatiles.typechange || clientPokemon?.volatiles.typeadd) {
 				text += `<small>(Type changed)</small><br />`;
 			}
-			text += types.map(type => Dex.getTypeIcon(type)).join(' ');
+			text += `<span class="textaligned-typeicons">${types.map(type => Dex.getTypeIcon(type)).join(' ')}</span>`;
 			if (pokemon.terastallized) {
-				text += ` <small>(base: ${this.getPokemonTypes(pokemon, true).map(type => Dex.getTypeIcon(type)).join(' ')})</small>`;
+				text += `&nbsp; &nbsp; <small>(base: <span class="textaligned-typeicons">${this.getPokemonTypes(pokemon, true).map(type => Dex.getTypeIcon(type)).join(' ')}</span>)</small>`;
 			} else if (serverPokemon?.teraType && !this.battle.rules['Terastal Clause']) {
-				text += ` <small>(Tera Type: ${Dex.getTypeIcon(serverPokemon.teraType)})</small>`;
+				text += `&nbsp; &nbsp; <small>(Tera Type: <span class="textaligned-typeicons">${Dex.getTypeIcon(serverPokemon.teraType)}</span>)</small>`;
 			}
 			text += `</h2>`;
 		}
