c35b8f18253ac2b1164e2088bc917bc976ebe250
Karthik <32044378+Karthik99999@users.noreply.github.com>
2023-01-20 17:28:01 -0500

    Fix Tera permanently replacing sprite (#2090)

--- src/battle-animations.ts
+++ src/battle-animations.ts
@@ -1477,6 +1477,9 @@ export class BattleScene implements BattleSceneStub {
 	animDragOut(pokemon: Pokemon) {
 		return pokemon.sprite.animDragOut(pokemon);
 	}
+	resetStatbar(pokemon: Pokemon, startHidden?: boolean) {
+		return pokemon.sprite.resetStatbar(pokemon, startHidden);
+	}
 	updateStatbar(pokemon: Pokemon, updatePrevhp?: boolean, updateHp?: boolean) {
 		return pokemon.sprite.updateStatbar(pokemon, updatePrevhp, updateHp);
 	}
