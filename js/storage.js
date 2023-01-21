ae9588b08e9b65d49c3c48c33d369fc143422fde
Karthik <32044378+Karthik99999@users.noreply.github.com>
2023-01-21 12:18:08 -0500

    Fix Tera Type not exporting when backing up all teams (#2094)

--- js/storage.js
+++ js/storage.js
@@ -646,7 +646,7 @@ Storage.unpackAllTeams = function (buffer) {
 	if (buffer.charAt(0) === '[' && $.trim(buffer).indexOf('\n') < 0) {
 		// old format
 		return JSON.parse(buffer).map(function (oldTeam) {
-			var format = oldTeam.format || 'gen8';
+			var format = oldTeam.format || 'gen9';
 			var capacity = 6;
 			if (format && format.slice(0, 3) !== 'gen') format = 'gen6' + format;
 			if (format && format.endsWith('-box')) {
@@ -656,6 +656,7 @@ Storage.unpackAllTeams = function (buffer) {
 			return {
 				name: oldTeam.name || '',
 				format: format,
+				gen: parseInt(format[3], 10) || 6,
 				team: Storage.packTeam(oldTeam.team),
 				capacity: capacity,
 				folder: '',
@@ -675,11 +676,12 @@ Storage.unpackLine = function (line) {
 	var isBox = line.slice(0, bracketIndex).endsWith('-box');
 	var slashIndex = line.lastIndexOf('/', pipeIndex);
 	if (slashIndex < 0) slashIndex = bracketIndex; // line.slice(slashIndex + 1, pipeIndex) will be ''
-	var format = bracketIndex > 0 ? line.slice(0, isBox ? bracketIndex - 4 : bracketIndex) : 'gen8';
+	var format = bracketIndex > 0 ? line.slice(0, isBox ? bracketIndex - 4 : bracketIndex) : 'gen9';
 	if (format && format.slice(0, 3) !== 'gen') format = 'gen6' + format;
 	return {
 		name: line.slice(slashIndex + 1, pipeIndex),
 		format: format,
+		gen: parseInt(format[3], 10) || 6,
 		team: line.slice(pipeIndex + 1),
 		capacity: isBox ? 24 : 6,
 		folder: line.slice(bracketIndex + 1, slashIndex > 0 ? slashIndex : bracketIndex + 1),
@@ -1116,7 +1118,7 @@ Storage.importTeam = function (buffer, teams) {
 		} else if (line.substr(0, 3) === '===' && teams) {
 			team = [];
 			line = $.trim(line.substr(3, line.length - 6));
-			var format = 'gen8';
+			var format = 'gen9';
 			var capacity = 6;
 			var bracketIndex = line.indexOf(']');
 			if (bracketIndex >= 0) {
@@ -1128,7 +1130,6 @@ Storage.importTeam = function (buffer, teams) {
 				}
 				line = $.trim(line.substr(bracketIndex + 1));
 			}
-			var gen = parseInt(format[3], 10) || 6;
 			if (teams.length && typeof teams[teams.length - 1].team !== 'string') {
 				teams[teams.length - 1].team = Storage.packTeam(teams[teams.length - 1].team);
 			}
@@ -1141,7 +1142,7 @@ Storage.importTeam = function (buffer, teams) {
 			teams.push({
 				name: line,
 				format: format,
-				gen: gen,
+				gen: parseInt(format[3], 10) || 6,
 				team: team,
 				capacity: capacity,
 				folder: folder,
@@ -1566,7 +1567,7 @@ Storage.nwLoadTeamFile = function (filename, localApp) {
 		return;
 	}
 
-	var format = 'gen8';
+	var format = 'gen9';
 	var capacity = 6;
 	var bracketIndex = line.indexOf(']');
 	if (bracketIndex >= 0) {
@@ -1584,6 +1585,7 @@ Storage.nwLoadTeamFile = function (filename, localApp) {
 			self.teams.push({
 				name: line,
 				format: format,
+				gen: parseInt(format[3], 10) || 6,
 				team: Storage.packTeam(Storage.importTeam('' + data)),
 				capacity: capacity,
 				folder: folder,
