9f1ef28ebbe5d0d34ee0a0fa940cd99b6253d4af
Karthik99999 <bandagondak0217@gmail.com>
2023-01-24 22:59:19 -0500

    Fix Cosmetic forme sprites not showing in Gen 9

--- js/client-teambuilder.js
+++ js/client-teambuilder.js
@@ -3430,7 +3430,7 @@
 			var spriteSize = 96;
 			var spriteDim = 'width: 96px; height: 96px;';
 
-			var gen = {1:'gen1', 2:'gen2', 3:'gen3', 4:'gen4', 5:'gen5', 6:'dex', 7:'dex', 8:'dex'}[Math.max(this.room.curTeam.gen, species.gen)];
+			var gen = {1:'gen1', 2:'gen2', 3:'gen3', 4:'gen4', 5:'gen5', 6:'dex', 7:'dex', 8:'dex', 9:'dex'}[Math.max(this.room.curTeam.gen, species.gen)];
 			if (Dex.prefs('nopastgens')) gen = 'dex';
 			if (Dex.prefs('bwgfx') && gen === 'dex') gen = 'gen5';
 			spriteDir += gen;
