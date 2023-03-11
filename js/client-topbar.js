6c619f7812b7606d8aeff9091d3c0b1d78c7f611
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-03-11 01:31:50 +0000

    capital T ofc

--- js/client-topbar.js
+++ js/client-topbar.js
@@ -686,7 +686,7 @@
 		},
 		initialize: function () {
 			var cur = this.chatformatting = Dex.prefs('chatformatting') || {};
-			cur.hideinterstice = True;
+			cur.hideinterstice = true;
 			var buf = '<p>Usable formatting:</p>';
 			var ctrlPlus = '<kbd>' + (navigator.platform === 'MacIntel' ? 'Cmd' : 'Ctrl') + '</kbd> + ';
 			buf += '<p class="optlabel">**<strong>bold</strong>** (' + ctrlPlus + '<kbd>B</kbd>)</p>';
