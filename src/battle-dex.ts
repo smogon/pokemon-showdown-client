0ac3be52df5c48aaad6c2e4c5e905d995b481877
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-03-12 15:52:38 +0000

    Revert "fix ability additions"
    This reverts commit 4f57446a8a00ed5e8ee8768280bcafcdf636edc6.

--- src/battle-dex.ts
+++ src/battle-dex.ts
@@ -922,12 +922,7 @@ class ModdedDex {
 				}
 			}
 			if (this.modid !== `gen${this.gen}`) {
-				let table;
-				if (this.modid === "insurgence") {
-					table = window.BattleTeambuilderTable["gen9insurgencenationaldex"];
-				} else {
-					table = window.BattleTeambuilderTable[this.modid];
-				}
+				const table = window.BattleTeambuilderTable[this.modid];
 				if (id in table.overrideAbilityData) {
 					Object.assign(data, table.overrideAbilityData[id]);
 				}
@@ -971,12 +966,7 @@ class ModdedDex {
 				data.abilities = {0: "No Ability"};
 			}
 
-			let table;
-			if (this.modid === "insurgence") {
-				table = window.BattleTeambuilderTable["gen9insurgencenationaldex"];
-			} else {
-				table = window.BattleTeambuilderTable[this.modid];
-			}
+			const table = window.BattleTeambuilderTable[this.modid];
 			if (id in table.overrideTier) data.tier = table.overrideTier[id];
 			if (!data.tier && id.slice(-5) === 'totem') {
 				data.tier = this.species.get(id.slice(0, -5)).tier;
