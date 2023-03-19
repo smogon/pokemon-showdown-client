b6216620d66d37de1493ce7a44a357f6f28e5112
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-03-19 08:32:59 +0000

    debug 3

--- src/battle-tooltips.ts
+++ src/battle-tooltips.ts
@@ -2199,8 +2199,7 @@ class BattleTooltips {
 			// for switch tooltips, only show the original ability
 			const ability = abilityData.baseAbility || abilityData.ability;
 			if (ability) text = '<small>Ability:</small> ' + Dex.abilities.get(ability).name;
-			console.log(this.battle.dex.species.get((serverPokemon?.speciesForme || '')));
-			console.log(this.battle.dex);
+			console.log(this.battle);
 		} else {
 			if (abilityData.ability) {
 				const abilityName = Dex.abilities.get(abilityData.ability).name;
