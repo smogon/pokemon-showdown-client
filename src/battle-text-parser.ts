8dc4d24dda447f0be6ec8ee05fd2698215a2a8bb
Karthik99999 <bandagondak0217@gmail.com>
2023-03-21 15:56:08 -0400

    say "backwards compatibility" instead of old replays

--- src/battle-text-parser.ts
+++ src/battle-text-parser.ts
@@ -857,7 +857,7 @@ class BattleTextParser {
 
 			if (id === 'commander') {
 				// Commander didn't have a message prior to v1.2.0 of SV
-				// so this is for old replays
+				// so this is for backwards compatibility
 				if (target === pokemon) return line1;
 				const template = this.template('activate', id);
 				return line1 + template.replace('[POKEMON]', this.pokemon(pokemon)).replace(/\[TARGET\]/g, this.pokemon(target));
