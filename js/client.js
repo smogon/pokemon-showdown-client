ab6e67efc0fa2134320e8c60d530e93944d1e3a7
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-03-27 17:41:25 +0100

    remove act from query

--- js/client.js
+++ js/client.js
@@ -218,7 +218,7 @@ function toId() {
 		getActionPHP: function () {
 			var ret = '/action.php?serverid={}';
 			if (Config.testclient) {
-				ret = 'https://play.pokemonshowdown.com/action.php?act=x';
+				ret = 'https://play.pokemonshowdown.com/action.php';
 			}
 			return (this.getActionPHP = function () {
 				return ret;
