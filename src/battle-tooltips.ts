60a255ecc8b8687185c99ddd79d39343a50f3e42
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-06-27 18:26:56 +0100

    remove that shit

--- src/battle-tooltips.ts
+++ src/battle-tooltips.ts
@@ -2220,9 +2220,6 @@ class BattleTooltips {
 		return value;
 	}
 	getPokemonTypes(pokemon: Pokemon | ServerPokemon, preterastallized = false): ReadonlyArray<TypeName> {
-		if (pokemon.speciesForme === "Giratina-Primal" && preterastallized) {
-			return ["???", "Bug", "Dark", "Dragon", "Electric", "Fairy", "Fighting", "Fire", "Flying", "Ghost", "Grass", "Ground", "Ice", "Normal", "Poison", "Psychic", "Rock", "Steel", "Water"];
-		}
 		if (!(pokemon as Pokemon).getTypes) {
 			return this.battle.dex.species.get(pokemon.speciesForme).types;
 		}
