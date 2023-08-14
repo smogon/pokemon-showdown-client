3f26ff58b5212d3429fc6da80eda4b7dc6292e3f
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-08-13 22:52:20 -0600

    Fix teambuilder support for Tatsugiri (#2135)

--- src/battle-dex-search.ts
+++ src/battle-dex-search.ts
@@ -744,6 +744,7 @@ abstract class BattleTypedSearch<T extends SearchType> {
 		if (lsetSpecies.id === 'gastrodoneast') return 'gastrodon' as ID;
 		if (lsetSpecies.id === 'pumpkaboosuper') return 'pumpkaboo' as ID;
 		if (lsetSpecies.id === 'sinisteaantique') return 'sinistea' as ID;
+		if (lsetSpecies.id === 'tatsugiristretchy') return 'tatsugiri' as ID;
 
 		const next = lsetSpecies.battleOnly || lsetSpecies.changesFrom || lsetSpecies.prevo;
 		if (next) return toID(next);
