d9d5db2c6d27dd1f820ec5a634ea7ab8be69e333
Alexander B <4866817+MathyFurret@users.noreply.github.com>
2023-09-01 11:35:50 -0500

    Ensure Revival Blessing only heals fainted Pokemon (#2142)

--- src/battle.ts
+++ src/battle.ts
@@ -1733,7 +1733,7 @@ export class Battle {
 			break;
 		}
 		case '-heal': {
-			let poke = this.getPokemon(args[1])!;
+			let poke = this.getPokemon(args[1], Dex.getEffect(kwArgs.from).id === 'revivalblessing')!;
 			let damage = poke.healthParse(args[2], true, true);
 			if (damage === null) break;
 			let range = poke.getDamageRange(damage);
@@ -3279,7 +3279,7 @@ export class Battle {
 		}
 		return null;
 	}
-	getPokemon(pokemonid: string | undefined) {
+	getPokemon(pokemonid: string | undefined, faintedOnly = false) {
 		if (!pokemonid || pokemonid === '??' || pokemonid === 'null' || pokemonid === 'false') {
 			return null;
 		}
@@ -3295,6 +3295,7 @@ export class Battle {
 
 		for (const pokemon of side.pokemon) {
 			if (isInactive && side.active.includes(pokemon)) continue;
+			if (faintedOnly && pokemon.hp) continue;
 			if (pokemon.ident === pokemonid) { // name matched, good enough
 				if (slot >= 0) pokemon.slot = slot;
 				return pokemon;
