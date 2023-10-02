38e1eaa9285b0cb8f2bfa4a7a635c5f2c556470a
Mia <49593536+mia-pi-git@users.noreply.github.com>
2023-10-02 09:47:10 -0500

    Caja: Whitelist custom attribute for formatselect element

--- src/battle-log.ts
+++ src/battle-log.ts
@@ -786,6 +786,7 @@ export class BattleLog {
 			'psicon::category': 0,
 			'username::name': 0,
 			'form::data-submitsend': 0,
+			'formatselect::format': 0,
 			'div::data-server': 0,
 			'button::data-send': 0,
 			'form::data-delimiter': 0,
