c07e40c5d3e39a66d3d23311cc211107b918280c
Karthik <32044378+Karthik99999@users.noreply.github.com>
2023-10-19 22:13:17 -0700

    Fix speed buttons on downloaded replays (#2162)

--- js/replay-embed.template.js
+++ js/replay-embed.template.js
@@ -81,7 +81,7 @@ var Replays = {
 			paused: true,
 		});
 
-		this.$('.replay-controls-2').html('<div class="chooser leftchooser speedchooser"> <em>Speed:</em> <div><button class="sel" value="fast">Fast</button><button value="normal">Normal</button><button value="slow">Slow</button><button value="reallyslow">Really Slow</button></div> </div> <div class="chooser colorchooser"> <em>Color&nbsp;scheme:</em> <div><button class="sel" value="light">Light</button><button value="dark">Dark</button></div> </div> <div class="chooser soundchooser" style="display:none"> <em>Music:</em> <div><button class="sel" value="on">On</button><button value="off">Off</button></div> </div>');
+		this.$('.replay-controls-2').html('<div class="chooser leftchooser speedchooser"> <em>Speed:</em> <div><button value="hyperfast">Hyperfast</button><button value="fast">Fast</button><button value="normal" class="sel">Normal</button><button value="slow">Slow</button><button value="reallyslow">Really Slow</button></div> </div> <div class="chooser colorchooser"> <em>Color&nbsp;scheme:</em> <div><button class="sel" value="light">Light</button><button value="dark">Dark</button></div> </div> <div class="chooser soundchooser" style="display:none"> <em>Music:</em> <div><button class="sel" value="on">On</button><button value="off">Off</button></div> </div>');
 
 		// this works around a WebKit/Blink bug relating to float layout
 		var rc2 = this.$('.replay-controls-2')[0];
@@ -138,13 +138,23 @@ var Replays = {
 			break;
 
 		case 'speed':
-			var speedTable = {
-				fast: 8,
-				normal: 800,
-				slow: 2500,
-				reallyslow: 5000
+			var fadeTable = {
+				hyperfast: 40,
+				fast: 50,
+				normal: 300,
+				slow: 500,
+				reallyslow: 1000
 			};
-			this.battle.messageDelay = speedTable[value];
+			var delayTable = {
+				hyperfast: 1,
+				fast: 1,
+				normal: 1,
+				slow: 1000,
+				reallyslow: 3000
+			};
+			this.battle.messageShownTime = delayTable[value];
+			this.battle.messageFadeTime = fadeTable[value];
+			this.battle.scene.updateAcceleration();
 			break;
 		}
 	},
