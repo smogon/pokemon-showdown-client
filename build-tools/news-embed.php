f5019425ab40c5c00c59d3a5b958db322ab84705
Guangcong Luo <guangcongluo@gmail.com>
2023-11-16 23:48:27 +0000

    Refactor build tools for readability
    (Also fixes the build error.)

--- build-tools/news-embed.php
+++ build-tools/news-embed.php
@@ -2,4 +2,4 @@
 date_default_timezone_set('America/Los_Angeles');
 include __DIR__.'/../pokemonshowdown.com/news/include.php';
 
-echo json_encode(array(getNewsId(), renderNews()));
+echo json_encode([getNewsId(), renderNews()]);
