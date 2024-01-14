1fadd5d587d9ea14792081411225282b20945b55
Hisuian Zoroark <96159984+HisuianZoroark@users.noreply.github.com>
2024-01-13 19:28:20 -0500

    Don't allow `|` in folder names (#2213)

--- play.pokemonshowdown.com/js/client-teambuilder.js
+++ play.pokemonshowdown.com/js/client-teambuilder.js
@@ -637,6 +637,10 @@
 							app.addPopupMessage("Names can't contain slashes, since they're used as a folder separator.");
 							name = name.replace(/[\\\/]/g, '');
 						}
+						if (name.indexOf('|') >= 0) {
+							app.addPopupMessage("Names can't contain the character |, since they're used for storing teams.");
+							name = name.replace(/\|/g, '');
+						}
 						if (!name) return;
 						self.selectFolder(name + '/');
 					}});
@@ -659,6 +663,10 @@
 				if (name.indexOf('/') >= 0 || name.indexOf('\\') >= 0) {
 					app.addPopupMessage("Names can't contain slashes, since they're used as a folder separator.");
 					name = name.replace(/[\\\/]/g, '');
+				}
+				if (name.indexOf('|') >= 0) {
+					app.addPopupMessage("Names can't contain the character |, since they're used for storing teams.");
+					name = name.replace(/\|/g, '');
 				}
 				if (!name) return;
 				if (name === oldFolder) return;
