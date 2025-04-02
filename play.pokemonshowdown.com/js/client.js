c5b146f41b5b3cb281a6a1fc7b6baf335fdbc71c
Guangcong Luo <guangcongluo@gmail.com>
2025-04-02 21:18:43 +0000

    April Fool's DLC: Add link

--- play.pokemonshowdown.com/js/client.js
+++ play.pokemonshowdown.com/js/client.js
@@ -1540,7 +1540,7 @@ function toId() {
 						var target = this.pathname.substr(1);
 
 						// keep this in sync with .htaccess
-						var shortLinks = /^(rooms?suggestions?|suggestions?|adminrequests?|forgotpassword|bugs?(reports?)?|formatsuggestions|rules?|faq|credits?|news|privacy|contact|dex|(damage)?calc|insecure|replays?|devdiscord|smogdex|smogcord|forums?)$/;
+						var shortLinks = /^(rooms?suggestions?|suggestions?|adminrequests?|forgotpassword|bugs?(reports?)?|formatsuggestions|rules?|faq|credits?|news|privacy|contact|dex|(damage)?calc|insecure|replays?|devdiscord|smogdex|smogcord|forums?|trustworthy\-dlc\-link)$/;
 						if (target === 'appeal' || target === 'appeals') target = 'view-help-request--appeal';
 						if (target === 'report') target = 'view-help-request--report';
 						if (target === 'requesthelp') target = 'view-help-request--other';
