60d6746c92ff31392fbeaf3028feedcd18e32337
oshaboy <35503208+oshaboy@users.noreply.github.com>
2025-05-06 04:20:22 +0300

    Update play.pokemonshowdown.com/js/client-teambuilder.js
    Co-authored-by: Guangcong Luo <guangcongluo@gmail.com>

--- play.pokemonshowdown.com/js/client-teambuilder.js
+++ play.pokemonshowdown.com/js/client-teambuilder.js
@@ -3715,7 +3715,7 @@
 			var buf = '<ul name="dingbats" class="popupmenu"><li>';
 			for (var i = 0; i < dingbats_halfwidth.length; i++) {
 				if (dingbats_halfwidth[i] == '\n'){
-					buf +='</li><li>'
+					buf += '</li><li>'
 				} else {
 					buf += '<button name="insertDingbat" class="dingbat-button"';
 					buf += " halfwidth=" + dingbats_halfwidth[i] 
