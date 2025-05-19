00abcf7c6b8081a394bd5c4443c5ff7bd877185e
Guangcong Luo <guangcongluo@gmail.com>
2025-05-19 00:56:27 +0000

    Preact minor updates batch 22
    - Refactor PS.send
      - No longer wants the leading `|` (omitting it is a common bug),
        making it work more like `room.send`.
    - Intercept links to teams and replays
    - Support replay fallback if battle room not found
    - New CopyableURLBox (for replays and teams)
    
    Teambuilder
    - Select species/ability/item/move text when clicking into them
      - (Regression from the upload button leak fix)
    - Don't let nicknames stop us from detecting teams unchanged since
      upload
    - "Compare" button for comparing local vs uploaded team
    - "Revert" button for reverting to uploaded team
    - View team page
    - Hide extraneous buttons from gen 1-2
    - Improve right-click and left-click behavior on import/export screen
    
    Trivial
    - `readonly` -> `readOnly` on TeamEditor, to match JSX
    - Refactor `connected`
    - Fix "Dynamax Level 10" appearing on imported teams

--- play.pokemonshowdown.com/src/battle-teams.ts
+++ play.pokemonshowdown.com/src/battle-teams.ts
@@ -256,12 +256,12 @@ export const Teams = new class {
 				if (i !== j) misc = buf.substring(i, j).split(',', 6);
 			}
 			if (misc) {
-				set.happiness = (misc[0] ? Number(misc[0]) : 255);
-				set.hpType = misc[1];
-				set.pokeball = misc[2];
-				set.gigantamax = !!misc[3];
-				set.dynamaxLevel = (misc[4] ? Number(misc[4]) : 10);
-				set.teraType = misc[5];
+				set.happiness = (misc[0] ? Number(misc[0]) : undefined);
+				set.hpType = misc[1] || undefined;
+				set.pokeball = misc[2] || undefined;
+				set.gigantamax = !!misc[3] || undefined;
+				set.dynamaxLevel = (misc[4] ? Number(misc[4]) : undefined);
+				set.teraType = misc[5] || undefined;
 			}
 			if (j < 0) break;
 			i = j + 1;
