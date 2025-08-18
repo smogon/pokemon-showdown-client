25da1cbbff5f56f91645fedd12c2adbb946222b6
Karthik99999 <bandagondak0217@gmail.com>
2025-08-18 15:14:34 -0400

    Fix Intimidate args order

--- play.pokemonshowdown.com/src/battle.ts
+++ play.pokemonshowdown.com/src/battle.ts
@@ -1671,7 +1671,7 @@ export class Battle {
 			if (args[0] === '-heal' && nextArgs[0] === '-heal' && kwArgs.from && kwArgs.from === nextKwargs.from) {
 				kwArgs.then = '.';
 			}
-			if (args[0] === '-ability' && (args[2] === 'Intimidate' || args[3] === 'boost')) {
+			if (args[0] === '-ability' && (args[2] === 'Intimidate' || args[4] === 'boost')) {
 				kwArgs.then = '.';
 			}
 			if (args[0] === '-unboost' && nextArgs[0] === '-unboost') {
