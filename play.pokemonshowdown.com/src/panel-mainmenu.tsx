b7886452a8ad2d1d99fbc58712ab0322d2e43724
Aurastic <33085835+ISenseAura@users.noreply.github.com>
2025-09-03 23:12:36 +0530

    Update play.pokemonshowdown.com/src/panel-mainmenu.tsx
    Co-authored-by: Mia <49593536+mia-pi-git@users.noreply.github.com>

--- play.pokemonshowdown.com/src/panel-mainmenu.tsx
+++ play.pokemonshowdown.com/src/panel-mainmenu.tsx
@@ -439,7 +439,7 @@ class NewsPanel extends PSRoomPanel {
 	change = (ev: Event) => {
 		const target = ev.currentTarget as HTMLInputElement;
 		if (target.value === '1') {
-			document.cookie = "preactalpha=1; expires=Thu, 1 Oct 2025 12:00:00 UTC; path=/";
+			document.cookie = "preactalpha=1; expires=Thu, 1 Oct 2035 12:00:00 UTC; path=/";
 		} else {
 			document.cookie = "preactalpha=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 		}
