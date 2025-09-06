e0c7f4a10f4b33e277ba97e34e522c7a22d8ee60
ShivaD173 <ShivaD173@users.noreply.github.com>
2025-09-06 16:25:10 -0400

    Do not set Gimmighoul-Roaming Atk IVs to 0 by default (#2490)

--- play.pokemonshowdown.com/js/client-teambuilder.js
+++ play.pokemonshowdown.com/js/client-teambuilder.js
@@ -3454,7 +3454,7 @@
 
 			var minAtk = true;
 			// only available through an event with 31 Atk IVs
-			if (set.ability === 'Battle Bond' || ['Koraidon', 'Miraidon'].includes(set.species)) minAtk = false;
+			if (set.ability === 'Battle Bond' || ['Koraidon', 'Miraidon', 'Gimmighoul-Roaming'].includes(set.species)) minAtk = false;
 			var hpModulo = (this.curTeam.gen >= 6 ? 2 : 4);
 			var hasHiddenPower = false;
 			var moves = set.moves;
