3c80827db86368fa3572223b9b268902941fe2a5
Karthik99999 <bandagondak0217@gmail.com>
2025-09-28 00:48:37 -0700

    Battle Text Parser: Add general support for past gen overrides

--- play.pokemonshowdown.com/src/battle-text-parser.ts
+++ play.pokemonshowdown.com/src/battle-text-parser.ts
@@ -8,7 +8,7 @@
  * @license MIT
  */
 
-import { toID, type ID } from "./battle-dex";
+import { Dex, toID, type ID } from "./battle-dex";
 
 export type Args = [string, ...string[]];
 export type KWArgs = { [kw: string]: string };
@@ -368,11 +368,21 @@ export class BattleTextParser {
 				if (BattleText[id][type].charAt(1) === '.') type = BattleText[id][type].slice(2) as ID;
 				if (BattleText[id][type].startsWith('#')) id = BattleText[id][type].slice(1) as ID;
 				if (!BattleText[id][type]) return '';
-				return BattleText[id][type] + '\n';
+				let template = BattleText[id][type];
+				for (let i = Dex.gen - 1; i >= this.gen; i--) {
+					let curTemplate = BattleText[id][`${type}Gen${i}`];
+					if (curTemplate) template = curTemplate;
+				}
+				return template + '\n';
 			}
 		}
 		if (!BattleText.default[type]) return '';
-		return BattleText.default[type] + '\n';
+		let template = BattleText.default[type];
+		for (let i = Dex.gen - 1; i >= this.gen; i--) {
+			let curTemplate = BattleText.default[`${type}Gen${i}`];
+			if (curTemplate) template = curTemplate;
+		}
+		return template + '\n';
 	}
 
 	maybeAbility(effect: string | undefined, holder: string) {
@@ -642,8 +652,6 @@ export class BattleTextParser {
 			if (kwArgs.damage) templateId = 'activate';
 			if (kwArgs.block) templateId = 'block';
 			if (kwArgs.upkeep) templateId = 'upkeep';
-			if (id === 'mist' && this.gen <= 2) templateId = `startGen${this.gen}`;
-			if (id === 'reflect' || id === 'lightscreen') templateId = 'startGen1';
 			if (templateId === 'start' && kwArgs.from?.startsWith('item:')) {
 				templateId += 'FromItem';
 			}
@@ -1079,10 +1087,7 @@ export class BattleTextParser {
 		case '-block': {
 			let [, pokemon, effect, move, attacker] = args;
 			const line1 = this.maybeAbility(effect, kwArgs.of || pokemon);
-			let id = BattleTextParser.effectId(effect);
-			let templateId = 'block';
-			if (id === 'mist' && this.gen <= 2) templateId = `blockGen${this.gen}`;
-			const template = this.template(templateId, effect);
+			const template = this.template('block', effect);
 			return line1 + template.replace('[POKEMON]', this.pokemon(pokemon))
 				.replace('[SOURCE]', this.pokemon(attacker || kwArgs.of)).replace('[MOVE]', move);
 		}
@@ -1158,7 +1163,6 @@ export class BattleTextParser {
 				id = 'dragonascent';
 				templateId = 'megaNoItem';
 			}
-			if (!id && cmd === '-mega' && this.gen < 7) templateId = 'megaGen6';
 			if (!item && cmd === '-mega') templateId = 'megaNoItem';
 			let template = this.template(templateId, id);
 			const side = pokemon.slice(0, 2);
