9019985662f4a37cf4a4c85a35835188f7fe3e0b
Daniel1464 <Daniel_Chen@caryacademy.org>
2025-12-05 06:56:22 +0000

    Preact: Fix timer countdown visual inaccuracy
    (split off from #2499)

--- play.pokemonshowdown.com/src/panel-battle.tsx
+++ play.pokemonshowdown.com/src/panel-battle.tsx
@@ -201,6 +201,7 @@ class TimerButton extends preact.Component<{ room: BattleRoom }> {
 		const room = this.props.room;
 		if (!this.timerInterval && room.battle.kickingInactive) {
 			this.timerInterval = setInterval(() => {
+				if (room.choices?.isDone()) return;
 				if (typeof room.battle.kickingInactive === 'number' && room.battle.kickingInactive > 1) {
 					room.battle.kickingInactive--;
 					if (room.battle.graceTimeLeft) room.battle.graceTimeLeft--;
