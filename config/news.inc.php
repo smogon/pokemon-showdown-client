c0fb5d41ec45d89d6ddfb0d2721ab2f646ed9f61
BlupBlurp <44441061+BlupBlurp@users.noreply.github.com>
2026-03-31 09:00:56 +0200

    Add client news

---
+++ config/news.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+$latestNewsCache = ['1', '2'];
+$newsCache = [
+'1' => [
+      'topic_id' => '1',
+      'title' => 'Welcome to Relumi Showdown',
+      'title_html' => 'Welcome to Relumi Showdown',
+      'authorname' => 'Blup',
+      'date' => 1774939138,
+      'summary' => 'This server features all custom balance changes exactly as seen in-game. Random Battle sets are also curated primarily from in-game teams.',
+      'summary_html' => '<p>This server features all custom balance changes exactly as seen in-game. Random Battle sets are also curated primarily from in-game teams.</p>',
+      'details' => '',
+      'details_html' => '',
+   ],
+	'2' => [
+		'topic_id' => '2',
+		'title' => 'Help us improve!',
+		'title_html' => 'Help us improve!',
+		'authorname' => 'Blup',
+		'date' => 1774939138,
+		'summary' => 'You can report any bugs or make any suggestions at: http://discord.gg/luminescent',
+		'summary_html' => '<p>You can report any bugs or make any suggestions at: <a href="http://discord.gg/luminescent" target="_blank">http://discord.gg/luminescent</a></p>',
+		'details' => '',
+		'details_html' => '',
+	],
+];
