d0df61b24287fb19f0ec97b15327c12d1d2ba240
ry <ryr4242@gmail.com>
2026-05-31 13:57:08 -0500

    different randbats name

--- play.pokemonshowdown.com/src/panel-mainmenu.tsx
+++ play.pokemonshowdown.com/src/panel-mainmenu.tsx
@@ -910,7 +910,7 @@ export class TeamForm extends preact.Component<{
 		if (window.BattleFormats) {
 			this.format ||= this.props.defaultFormat || '';
 			if (!this.format) {
-				this.format = `gen${Dex.gen}randombattle`;
+				this.format = `gen${Dex.gen}swserandombattle`;
 
 				const starredPrefs = PS.prefs.starredformats || {};
 				// .reverse() because the newest starred format should be the default one
