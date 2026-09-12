e47b8be4103b5e027cd191a024e383be88f37bfe
Guangcong Luo <guangcongluo@gmail.com>
2026-09-11 22:51:35 -0700

    Fix build

--- build-tools/compiler.mjs
+++ build-tools/compiler.mjs
@@ -182,7 +182,7 @@ function compileToDir(srcDir, destDir, opts = {}) {
 	return total;
 }
 
-function compileToFile(srcFile, destFile, opts) {
+async function compileToFile(srcFile, destFile, opts) {
 	const incremental = opts.incremental;
 	delete opts.incremental;
 
@@ -205,12 +205,11 @@ function compileToFile(srcFile, destFile, opts) {
 		if (VERBOSE) console.log(src + " ->");
 	}
 
-	combineResults(results, {
+	const combined = await combineResults(results, {
 		file: path.basename(destFile),
 		sourceRoot: opts.sourceRoot,
-	}, opts).then(combined => {
-		outputFileSync(destFile, combined, opts);
-	});
+	}, opts);
+	outputFileSync(destFile, combined, opts);
 
 	if (VERBOSE) console.log("-> " + destFile);
 	if (incremental) opts.incremental = true; // incredibly dumb hack to preserve the option
